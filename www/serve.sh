ddc3e53c1bf7f80c3282534d1ffa93e4544e3a68
Rich Lane <lanerl@gmail.com>
2021-07-04 09:41:41 -0700

    www: fix build target

--- www/serve.sh
+++ www/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
-cargo build
+cargo build --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/debug/oort.wasm www/
 which basic-http-server || cargo install basic-http-server
 basic-http-server www
