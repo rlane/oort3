64ced7597534c9848228fc33ba94182ed19dff8e
Rich Lane <lanerl@gmail.com>
2021-07-04 19:06:38 -0700

    factor out simulate and render

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use macroquad::input::KeyCode;
-use macroquad::math::vec2;
+use macroquad::math::{vec2, Vec2};
 use macroquad::{audio, camera, color, input, rand, shapes, text, window};
 
 struct Ball {
@@ -10,20 +10,21 @@ struct Ball {
     r: f32,
 }
 
+const WORLD_SIZE: f32 = 1000.0;
+
 #[macroquad::main("Oort")]
 async fn main() {
     let mut balls = vec![];
     let collision_sound = audio::load_sound("assets/collision.wav").await.unwrap();
     let mut zoom = 0.001;
     let mut camera_target = vec2(0.0, 0.0);
-    let world_size = 1000.0;
 
     for _ in 0..10 {
         let r = rand::gen_range(10.0, 20.0);
         let s = 10.0;
         balls.push(Ball {
-            x: rand::gen_range(r - world_size / 2.0, world_size / 2.0 - r),
-            y: rand::gen_range(r - world_size / 2.0, world_size / 2.0 - r),
+            x: rand::gen_range(r - WORLD_SIZE / 2.0, WORLD_SIZE / 2.0 - r),
+            y: rand::gen_range(r - WORLD_SIZE / 2.0, WORLD_SIZE / 2.0 - r),
             vx: rand::gen_range(-s, s),
             vy: rand::gen_range(-s, s),
             r: r,
@@ -54,75 +55,8 @@ async fn main() {
             break;
         }
 
-        window::clear_background(color::BLACK);
-
-        camera::set_camera(&camera::Camera2D {
-            zoom: vec2(
-                zoom,
-                zoom * window::screen_width() / window::screen_height(),
-            ),
-            target: camera_target,
-            ..Default::default()
-        });
-
-        let grid_size = 100.0;
-        let n = 1 + (world_size / grid_size) as i32;
-        for i in -(n / 2)..(n / 2 + 1) {
-            shapes::draw_line(
-                (i as f32) * grid_size,
-                -world_size / 2.0,
-                (i as f32) * grid_size,
-                world_size / 2.0,
-                1.0,
-                color::GREEN,
-            );
-            shapes::draw_line(
-                -world_size / 2.0,
-                (i as f32) * grid_size,
-                world_size / 2.0,
-                (i as f32) * grid_size,
-                1.0,
-                color::GREEN,
-            );
-        }
-
-        {
-            let v = -world_size / 2.0;
-            shapes::draw_line(-v, -v, v, -v, 1.0, color::RED);
-            shapes::draw_line(-v, v, v, v, 1.0, color::RED);
-            shapes::draw_line(-v, -v, -v, v, 1.0, color::RED);
-            shapes::draw_line(v, -v, v, v, 1.0, color::RED);
-        }
-
-        for ball in &mut balls {
-            ball.x += ball.vx;
-            ball.y += ball.vy;
-
-            if ball.x - ball.r < -world_size / 2.0 || ball.x + ball.r > world_size / 2.0 {
-                ball.vx *= -1.0;
-            }
-
-            if ball.y - ball.r < -world_size / 2.0 || ball.y + ball.r > world_size / 2.0 {
-                ball.vy *= -1.0;
-            }
-
-            shapes::draw_circle(ball.x, ball.y, ball.r, color::YELLOW);
-        }
-
-        let n = balls.len();
-        for i in 0..n {
-            for j in (i + 1)..n {
-                let dist_squared =
-                    (balls[i].x - balls[j].x).powf(2.0) + (balls[i].y - balls[j].y).powf(2.0);
-                if dist_squared < (balls[i].r + balls[j].r).powf(2.0) {
-                    balls[i].vx *= -1.0;
-                    balls[i].vy *= -1.0;
-                    balls[j].vx *= -1.0;
-                    balls[j].vy *= -1.0;
-                    audio::play_sound_once(collision_sound);
-                }
-            }
-        }
+        simulate(&mut balls, collision_sound);
+        render(camera_target, zoom, &balls);
 
         window::next_frame().await
     }
@@ -135,4 +69,80 @@ async fn main() {
         100.0,
         color::RED,
     );
+}
+
+fn simulate(balls: &mut Vec<Ball>, collision_sound: audio::Sound) {
+    for ball in balls.iter_mut() {
+        ball.x += ball.vx;
+        ball.y += ball.vy;
+
+        if ball.x - ball.r < -WORLD_SIZE / 2.0 || ball.x + ball.r > WORLD_SIZE / 2.0 {
+            ball.vx *= -1.0;
+        }
+
+        if ball.y - ball.r < -WORLD_SIZE / 2.0 || ball.y + ball.r > WORLD_SIZE / 2.0 {
+            ball.vy *= -1.0;
+        }
+    }
+
+    let n = balls.len();
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let dist_squared =
+                (balls[i].x - balls[j].x).powf(2.0) + (balls[i].y - balls[j].y).powf(2.0);
+            if dist_squared < (balls[i].r + balls[j].r).powf(2.0) {
+                balls[i].vx *= -1.0;
+                balls[i].vy *= -1.0;
+                balls[j].vx *= -1.0;
+                balls[j].vy *= -1.0;
+                audio::play_sound_once(collision_sound);
+            }
+        }
+    }
+}
+
+fn render(camera_target: Vec2, zoom: f32, balls: &[Ball]) {
+    window::clear_background(color::BLACK);
+
+    camera::set_camera(&camera::Camera2D {
+        zoom: vec2(
+            zoom,
+            zoom * window::screen_width() / window::screen_height(),
+        ),
+        target: camera_target,
+        ..Default::default()
+    });
+
+    let grid_size = 100.0;
+    let n = 1 + (WORLD_SIZE / grid_size) as i32;
+    for i in -(n / 2)..(n / 2 + 1) {
+        shapes::draw_line(
+            (i as f32) * grid_size,
+            -WORLD_SIZE / 2.0,
+            (i as f32) * grid_size,
+            WORLD_SIZE / 2.0,
+            1.0,
+            color::GREEN,
+        );
+        shapes::draw_line(
+            -WORLD_SIZE / 2.0,
+            (i as f32) * grid_size,
+            WORLD_SIZE / 2.0,
+            (i as f32) * grid_size,
+            1.0,
+            color::GREEN,
+        );
+    }
+
+    {
+        let v = -WORLD_SIZE / 2.0;
+        shapes::draw_line(-v, -v, v, -v, 1.0, color::RED);
+        shapes::draw_line(-v, v, v, v, 1.0, color::RED);
+        shapes::draw_line(-v, -v, -v, v, 1.0, color::RED);
+        shapes::draw_line(v, -v, v, v, 1.0, color::RED);
+    }
+
+    for ball in balls {
+        shapes::draw_circle(ball.x, ball.y, ball.r, color::YELLOW);
+    }
 }
