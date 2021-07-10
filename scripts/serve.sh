c990d8df4bdf6e6578ea319928dc2880017202f2
Rich Lane <lanerl@gmail.com>
2021-07-09 23:19:26 -0700

    build in release mode

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
-./scripts/wasm-bindgen-macroquad.sh oort
+./scripts/wasm-bindgen-macroquad.sh --release oort
 which basic-http-server || cargo install basic-http-server
 basic-http-server www
