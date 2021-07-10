2cc4b64b6aa9ec59e79c44c8b5fcdafaa08d4852
Rich Lane <lanerl@gmail.com>
2021-07-10 02:06:03 -0700

    remove use of macroquad::input

--- src/bin/oort.rs
+++ src/bin/oort.rs
@@ -1,7 +1,10 @@
-use macroquad::input::KeyCode;
-use macroquad::{input, shapes, window};
+use macroquad::{shapes, window};
 use nalgebra::point;
 use oort::{frame_timer, renderer, simulation};
+use std::sync::mpsc;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{console, KeyboardEvent};
 
 #[macroquad::main("Oort")]
 async fn main() {
@@ -13,6 +16,25 @@ async fn main() {
         .get_element_by_id("status")
         .expect("should have a status div");
     status_div.set_inner_html("Hello from Rust");
+
+    let canvas = document
+        .get_element_by_id("glcanvas")
+        .expect("expecting a canvas");
+
+    let (key_tx, key_rx) = mpsc::channel::<KeyboardEvent>();
+    let key_callback = Closure::wrap(Box::new(move |e: web_sys::KeyboardEvent| {
+        if key_tx.send(e).is_err() {
+            console::log_1(&"Failed to enqueue key".into());
+        }
+    }) as Box<dyn FnMut(_)>);
+
+    canvas
+        .add_event_listener_with_callback("keydown", key_callback.as_ref().unchecked_ref())
+        .expect("adding event listener failed");
+    canvas
+        .add_event_listener_with_callback("keyup", key_callback.as_ref().unchecked_ref())
+        .expect("adding event listener failed");
+    key_callback.forget();
 
     let mut sim = simulation::Simulation::new();
     let mut renderer = renderer::Renderer::new();
@@ -26,34 +48,45 @@ async fn main() {
     let scenario = oort::scenario::load("asteroid");
     scenario.init(&mut sim);
 
+    let mut keys_down = std::collections::HashSet::<String>::new();
+    let mut keys_ignored = std::collections::HashSet::<String>::new();
+
     loop {
         let mut status_msgs: Vec<String> = Vec::new();
 
         frame_timer.start("frame");
 
+        while let Ok(e) = key_rx.try_recv() {
+            if e.type_() == "keydown" {
+                keys_down.insert(e.key());
+            } else if e.type_() == "keyup" {
+                keys_down.remove(&e.key());
+                keys_ignored.remove(&e.key());
+            }
+        }
+
         let camera_step = 0.01 / zoom;
-        if input::is_key_down(KeyCode::W) {
+        let is_key_down = |key: &str| keys_down.contains(key);
+        if is_key_down("w") {
             camera_target.y += camera_step;
         }
-        if input::is_key_down(KeyCode::S) {
+        if is_key_down("s") {
             camera_target.y -= camera_step;
         }
-        if input::is_key_down(KeyCode::A) {
+        if is_key_down("a") {
             camera_target.x -= camera_step;
         }
-        if input::is_key_down(KeyCode::D) {
+        if is_key_down("d") {
             camera_target.x += camera_step;
         }
-        if input::is_key_down(KeyCode::Z) {
+        if is_key_down("z") {
             zoom *= 0.99;
         }
-        if input::is_key_down(KeyCode::X) {
+        if is_key_down("x") {
             zoom *= 1.01;
         }
-        if input::is_key_down(KeyCode::Q) | input::is_key_down(KeyCode::Escape) {
-            break;
-        }
-        if input::is_key_pressed(KeyCode::U) {
+        if is_key_down("u") && !keys_ignored.contains("u") {
+            keys_ignored.insert("u".to_string());
             for name in frame_timer.get_names() {
                 let (a, b, c) = frame_timer.get(name);
                 println!("{}: {:.1}/{:.1}/{:.1} ms", name, a * 1e3, b * 1e3, c * 1e3);
@@ -64,11 +97,13 @@ async fn main() {
                 sim.bullets.iter().count()
             );
         }
-        if input::is_key_pressed(KeyCode::Space) {
+        if is_key_down(" ") && !keys_ignored.contains(" ") {
+            keys_ignored.insert(" ".to_string());
             paused = !paused;
             single_steps = 0;
         }
-        if input::is_key_pressed(KeyCode::N) {
+        if is_key_down("n") && !keys_ignored.contains("n") {
+            keys_ignored.insert("n".to_string());
             paused = true;
             single_steps += 1;
         }
@@ -76,33 +111,33 @@ async fn main() {
         if !paused {
             if let Some(&ship_handle) = sim.ships.iter().next() {
                 let force = 1e4;
-                if input::is_key_down(KeyCode::Up) {
+                if is_key_down("ArrowUp") {
                     sim.ship_mut(ship_handle).thrust_main(force);
                 }
-                if input::is_key_down(KeyCode::Down) {
+                if is_key_down("ArrowDown") {
                     sim.ship_mut(ship_handle).thrust_main(-force);
                 }
-                if input::is_key_down(KeyCode::Left) {
-                    if input::is_key_down(KeyCode::LeftShift) {
+                if is_key_down("ArrowLeft") {
+                    if is_key_down("Shift") {
                         sim.ship_mut(ship_handle).thrust_lateral(force);
                     } else {
                         sim.ship_mut(ship_handle).thrust_angular(force);
                     }
                 }
-                if input::is_key_down(KeyCode::Right) {
-                    if input::is_key_down(KeyCode::LeftShift) {
+                if is_key_down("ArrowRight") {
+                    if is_key_down("Shift") {
                         sim.ship_mut(ship_handle).thrust_lateral(-force);
                     } else {
                         sim.ship_mut(ship_handle).thrust_angular(-force);
                     }
                 }
-                if input::is_key_pressed(KeyCode::F) {
+                if is_key_down("f") {
                     sim.ship_mut(ship_handle).fire_weapon();
                 }
-                if input::is_key_down(KeyCode::LeftShift) && input::is_key_down(KeyCode::F) {
+                if is_key_down("Shift") && is_key_down("f") {
                     sim.ship_mut(ship_handle).fire_weapon();
                 }
-                if input::is_key_down(KeyCode::LeftShift) && input::is_key_pressed(KeyCode::K) {
+                if is_key_down("Shift") && is_key_down("k") {
                     sim.ship_mut(ship_handle).explode();
                 }
             }
