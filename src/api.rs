c70461b9c86ab297f667ebd19d7def861d614212
Rich Lane <richlane@google.com>
2021-07-21 21:41:50 -0700

    api: prevent repeat panics

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,18 @@
 use crate::ui::telemetry;
 use crate::ui::UI;
 use lazy_static::lazy_static;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 
 lazy_static! {
     static ref OORT_UI: Mutex<Option<UI>> = Mutex::new(None);
+}
+
+static PANICKED: AtomicBool = AtomicBool::new(false);
+
+fn has_panicked() -> bool {
+    PANICKED.load(Ordering::SeqCst)
 }
 
 #[wasm_bindgen]
@@ -15,6 +22,7 @@ pub fn initialize() {
         telemetry::send(telemetry::Telemetry::Crash {
             msg: panic_info.to_string(),
         });
+        PANICKED.store(true, Ordering::SeqCst);
     }));
     console_log::init_with_level(log::Level::Info).expect("initializing logging");
     log::info!("Version {}", &crate::version());
@@ -22,12 +30,18 @@ pub fn initialize() {
 
 #[wasm_bindgen]
 pub fn start(scenario_name: &str) {
+    if has_panicked() {
+        return;
+    }
     let mut ui = OORT_UI.lock().unwrap();
     *ui = Some(UI::new(scenario_name));
 }
 
 #[wasm_bindgen]
 pub fn render() {
+    if has_panicked() {
+        return;
+    }
     let mut ui = OORT_UI.lock().unwrap();
     if ui.is_some() {
         ui.as_mut().unwrap().render();
@@ -36,6 +50,9 @@ pub fn render() {
 
 #[wasm_bindgen]
 pub fn on_key_event(e: web_sys::KeyboardEvent) {
+    if has_panicked() {
+        return;
+    }
     let mut ui = OORT_UI.lock().unwrap();
     if ui.is_some() {
         ui.as_mut().unwrap().on_key_event(e);
@@ -44,6 +61,9 @@ pub fn on_key_event(e: web_sys::KeyboardEvent) {
 
 #[wasm_bindgen]
 pub fn on_wheel_event(e: web_sys::WheelEvent) {
+    if has_panicked() {
+        return;
+    }
     let mut ui = OORT_UI.lock().unwrap();
     if ui.is_some() {
         ui.as_mut().unwrap().on_wheel_event(e);
@@ -52,6 +72,9 @@ pub fn on_wheel_event(e: web_sys::WheelEvent) {
 
 #[wasm_bindgen]
 pub fn upload_code(code: &str) {
+    if has_panicked() {
+        return;
+    }
     let mut ui = OORT_UI.lock().unwrap();
     if ui.is_some() {
         ui.as_mut().unwrap().upload_code(code);
@@ -60,6 +83,9 @@ pub fn upload_code(code: &str) {
 
 #[wasm_bindgen]
 pub fn get_initial_code() -> String {
+    if has_panicked() {
+        return "".to_string();
+    }
     let mut ui = OORT_UI.lock().unwrap();
     if ui.is_some() {
         ui.as_mut().unwrap().get_initial_code()
