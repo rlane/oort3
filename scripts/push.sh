fe94789de78343498bca74551a497cc886004b6a
Rich Lane <richlane@google.com>
2021-07-21 21:03:24 -0700

    scripts: clean build before push

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 BUCKET=oort-bin
 cd $(realpath $(dirname $0))/../www
+rm -rf ../target/wasm32-unknown-unknown/release/build/oort-* ../target/wasm32-unknown-unknown/release/*oort.*
 npm run build
 gsutil -m rsync dist/. gs://$BUCKET/
 : Pushed to https://storage.googleapis.com/$BUCKET/index.html
