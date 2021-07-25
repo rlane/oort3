01dfbea9e74819709c36ee52a31772a826f0d5b5
Rich Lane <richlane@google.com>
2021-07-24 19:03:20 -0700

    test: speed up tutorial_test with rayon

--- tests/tutorial_test.rs
+++ tests/tutorial_test.rs
@@ -1,9 +1,10 @@
 use oort::simulation;
 use oort::simulation::scenario;
+use rayon::prelude::*;
 use test_env_log::test;
 
 fn check_solution(scenario_name: &str) {
-    for _ in 0..10 {
+    let check_once = || {
         let mut sim = simulation::Simulation::new();
         let mut scenario = scenario::load(scenario_name);
         scenario.init(&mut sim);
@@ -17,7 +18,8 @@ fn check_solution(scenario_name: &str) {
         }
 
         assert_eq!(scenario.status(&sim), scenario::Status::Finished);
-    }
+    };
+    (0..10usize).into_par_iter().for_each(|_| check_once());
 }
 
 #[test]
