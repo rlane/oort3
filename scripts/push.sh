7d432ad8afc6094320e395a5a4617d84b6af9112
Rich Lane <richlane@google.com>
2021-07-26 09:58:19 -0700

    use fnm to manage node versions

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 BUCKET=oort-bin
 cd $(realpath $(dirname $0))/../www
+if which fnm >/dev/null; then
+  eval "$(fnm env)"
+fi
+set +x
 rm -rf ../target/wasm32-unknown-unknown/release/build/oort-* ../target/wasm32-unknown-unknown/release/*oort.*
 npm run build
 gsutil -m rsync dist/. gs://$BUCKET/
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,3 +1,7 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 cd $(realpath $(dirname $0))/../www
+if which fnm >/dev/null; then
+  eval "$(fnm env)"
+fi
+set +x
 npm run serve
