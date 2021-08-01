5bd8971845893feb63fefdcbb7e86b6760573907
Rich Lane <richlane@google.com>
2021-08-01 10:57:46 -0700

    sim: add missing collision.rs

---
+++ src/simulation/collision.rs
@@ -0,0 +1,43 @@
+use super::ship::ShipClass;
+use rapier2d_f64::prelude::*;
+
+const WALL_COLLISION_GROUP: u32 = 0;
+const SHIP_COLLISION_GROUP: u32 = 1;
+const MAX_TEAMS: u32 = 10;
+
+fn bullet_group(team: i32) -> u32 {
+    1 << (2 + team)
+}
+
+fn all_bullet_groups() -> u32 {
+    let mut r: u32 = 0;
+    for team in 0..(MAX_TEAMS as i32) {
+        r |= bullet_group(team);
+    }
+    r
+}
+
+pub fn bullet_interaction_groups(team: i32) -> InteractionGroups {
+    InteractionGroups::new(
+        bullet_group(team),
+        1 << WALL_COLLISION_GROUP | 1 << SHIP_COLLISION_GROUP,
+    )
+}
+
+pub fn wall_interaction_groups() -> InteractionGroups {
+    InteractionGroups::new(
+        1 << WALL_COLLISION_GROUP,
+        1 << SHIP_COLLISION_GROUP | all_bullet_groups(),
+    )
+}
+
+pub fn ship_interaction_groups(team: i32, class: ShipClass) -> InteractionGroups {
+    let bullet_groups = match class {
+        ShipClass::Missile => all_bullet_groups() & !bullet_group(team),
+        _ => all_bullet_groups(),
+    };
+    InteractionGroups::new(
+        1 << SHIP_COLLISION_GROUP,
+        1 << WALL_COLLISION_GROUP | 1 << SHIP_COLLISION_GROUP | bullet_groups,
+    )
+}
