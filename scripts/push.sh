fd3243f4eadb7c7ba4e5a6c4655f5d51ef343c8a
Rich Lane <richlane@google.com>
2022-05-30 21:18:37 -0700

    scripts: build in release mode sequentially

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,8 +2,14 @@
 eval "$(fnm env)"
 set -x
 
-cd $(realpath $(dirname $0))/../app
-rm -rf dist/ ../target/wasm32-unknown-unknown/release/build/oort-* ../target/wasm32-unknown-unknown/release/*oort.*
+cd $(realpath $(dirname $0))/..
+rm -rf target/wasm32-unknown-unknown/release
+for PKG in oort_simulator oort_worker oort_renderer oort-app; do
+  cargo build --target wasm32-unknown-unknown --release --package $PKG
+done
+
+cd app
+rm -rf dist
 trunk build --release
 
 cd ../backend
