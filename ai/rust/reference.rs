8f4c5c3a925bb5afd9e6b2fe4c9e320d638b99e4
Rich Lane <richlane@google.com>
2022-06-15 23:20:37 -0700

    wasm: support I/O via state array

--- ai/rust/reference.rs
+++ ai/rust/reference.rs
@@ -1,4 +1,25 @@
 #[no_mangle]
+pub static mut SYSTEM_STATE: [f64; 2] = [
+    42.0,
+    21.0
+];
+
+pub fn write_system_state(index: usize, value: f64) {
+    unsafe {
+        SYSTEM_STATE[index] = value;
+    }
+}
+
+mod api {
+    use super::write_system_state;
+
+    pub fn accelerate(x: f64, y: f64) {
+        write_system_state(0, x );
+        write_system_state(1, y );
+    }
+}
+
+#[no_mangle]
 pub fn export_tick() {
     tick();
 }
@@ -6,4 +27,5 @@ pub fn export_tick() {
 // User code
 
 pub fn tick() {
+    api::accelerate(1.0, 2.0);
 }
--- simulator/src/script/wasm/mod.rs
+++ simulator/src/script/wasm/mod.rs
@@ -1,7 +1,7 @@
 use super::{ShipController, TeamController};
 use crate::ship::ShipHandle;
 use crate::simulation::Simulation;
-use js_sys::{Function, Object, Reflect, Uint8Array, WebAssembly};
+use js_sys::{Float64Array, Function, Object, Reflect, Uint8Array, WebAssembly};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -27,27 +27,53 @@ impl WasmTeamController {
 impl TeamController for WasmTeamController {
     fn create_ship_controller(
         &mut self,
-        _handle: ShipHandle,
-        _sim: &mut Simulation,
+        handle: ShipHandle,
+        sim: &mut Simulation,
         _orders: String,
     ) -> Result<Box<dyn ShipController>, super::Error> {
         let instance: WebAssembly::Instance =
             WebAssembly::Instance::new(&self.module, &Object::new())?;
-        Ok(Box::new(WasmShipController { instance }))
+        let exports = instance.exports();
+
+        let system_state_global: WebAssembly::Global =
+            Reflect::get(exports.as_ref(), &"SYSTEM_STATE".into())?.dyn_into()?;
+        let system_state_offset = system_state_global.value().as_f64().unwrap() as u32;
+
+        let memory: WebAssembly::Memory =
+            Reflect::get(exports.as_ref(), &"memory".into())?.dyn_into()?;
+        let buffer = memory.buffer();
+        let system_state =
+            Float64Array::new_with_byte_offset_and_length(&buffer, system_state_offset, 2);
+
+        let tick = Reflect::get(exports.as_ref(), &"export_tick".into())?
+            .dyn_into::<Function>()
+            .expect("export_tick wasn't a function");
+
+        Ok(Box::new(WasmShipController {
+            handle,
+            sim,
+            system_state,
+            tick,
+        }))
     }
 }
 
 struct WasmShipController {
-    pub instance: WebAssembly::Instance,
+    pub handle: ShipHandle,
+    pub sim: *mut Simulation,
+    pub system_state: Float64Array,
+    pub tick: Function,
 }
 
 impl ShipController for WasmShipController {
     fn tick(&mut self) -> Result<(), super::Error> {
-        let exports = self.instance.exports();
-        let tick = Reflect::get(exports.as_ref(), &"export_tick".into())?
-            .dyn_into::<Function>()
-            .expect("export_tick wasn't a function");
-        tick.call0(&JsValue::undefined())?;
+        //log::info!("before: system state: {:?}", self.system_state.to_vec());
+        self.tick.call0(&JsValue::undefined())?;
+        //log::info!("after:  system state: {:?}", self.system_state.to_vec());
+        let system_state = self.system_state.to_vec();
+        let sim = unsafe { &mut *self.sim };
+        sim.ship_mut(self.handle)
+            .accelerate(Vec2::new(system_state[0], system_state[1]));
         Ok(())
     }
 
