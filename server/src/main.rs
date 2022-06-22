6f820d5bd37891030eb2ce04c7f9deef182df5dc
Rich Lane <richlane@google.com>
2022-06-21 18:55:46 -0700

    server: improve error handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,38 @@
 use bytes::Bytes;
 use salvo::prelude::*;
-use std::process::Command;
+use tokio::process::Command;
+
+async fn compile_internal(req: &mut Request, res: &mut Response) -> anyhow::Result<()> {
+    log::debug!("Got request {:?}", req);
+    let payload = req.payload().await?;
+    let code = std::str::from_utf8(payload)?;
+    log::debug!("Code: {}", code);
+    std::fs::write("ai/src/user.rs", payload)?;
+    let start_time = std::time::Instant::now();
+    let output = Command::new("./scripts/build-ai.sh").output().await?;
+    let elapsed = std::time::Instant::now() - start_time;
+    if !output.status.success() {
+        log::info!("Compile failed in {:?}", elapsed);
+        let stdout = std::str::from_utf8(&output.stdout)?;
+        let stderr = std::str::from_utf8(&output.stderr)?;
+        log::debug!("Compile failed: stderr={}\nstdout={}", stderr, stdout);
+        res.render(stdout.to_string());
+        return Ok(());
+    }
+    log::info!("Compile succeeded in {:?}", elapsed);
+    log::debug!("Compile finished: {}", std::str::from_utf8(&output.stderr)?);
+    let wasm = std::fs::read("target/wasm32-unknown-unknown/release/oort_reference_ai.wasm")?;
+    res.write_body(Bytes::copy_from_slice(&wasm))?;
+    Ok(())
+}
 
 #[fn_handler]
 async fn compile(req: &mut Request, res: &mut Response) {
-    log::info!("Got request {:?}", req);
-    let payload = req.payload().await.expect("Reading payload failed");
-    let slice: &[u8] = payload;
-    let code = std::str::from_utf8(slice).unwrap();
-    log::debug!("Code: {}", code);
-
-    std::fs::write("ai/src/user.rs", slice).unwrap();
-
-    let output = Command::new("./scripts/build-ai.sh").output().unwrap();
-
-    if !output.status.success() {
-        log::info!(
-            "Compile failed: {}",
-            std::str::from_utf8(&output.stderr).unwrap()
-        );
-        return;
+    if let Err(e) = compile_internal(req, res).await {
+        log::error!("compile request error: {}", e);
+        res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
+        res.render(e.to_string().to_string());
     }
-
-    log::info!(
-        "Compile finished: {}",
-        std::str::from_utf8(&output.stderr).unwrap()
-    );
-
-    let wasm =
-        std::fs::read("target/wasm32-unknown-unknown/release/oort_reference_ai.wasm").unwrap();
-
-    res.write_body(Bytes::copy_from_slice(&wasm))
-        .expect("Response::write_body failed");
 }
 
 #[tokio::main]
@@ -51,7 +52,7 @@ async fn main() {
         Err(_e) => {}
     };
     let router = Router::with_path("compile").post(compile);
-    log::info!("Starting oort_server 1");
+    log::info!("Starting oort_server");
     Server::new(TcpListener::bind(&format!("0.0.0.0:{}", port)))
         .serve(router)
         .await;
