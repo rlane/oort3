1ba8ee972deacfa912a63cea19d82860630cb2bb
Rich Lane <richlane@google.com>
2022-06-22 20:07:56 -0700

    server: increase RAM

--- scripts/deploy-oort-server.sh
+++ scripts/deploy-oort-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -eux
 PROJECT=us-west1-docker.pkg.dev/oort-319301
-gcloud run deploy oortserver --image $PROJECT/oortserver/oortserver --allow-unauthenticated --region=us-west1
+gcloud run deploy oortserver \
+  --image $PROJECT/oortserver/oortserver \
+  --allow-unauthenticated \
+  --region=us-west1 \
+  --memory 2G
