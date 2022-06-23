879b4397152154fc252cc826e2dc2dbc9a26738d
Rich Lane <richlane@google.com>
2022-06-22 22:25:50 -0700

    server: combine push and deploy scripts

--- scripts/deploy-oort-server.sh
+++ scripts/deploy-oort-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
+cd $(realpath $(dirname $0))/..
 PROJECT=us-west1-docker.pkg.dev/oort-319301
+docker tag oort_server:latest $PROJECT/oortserver/oortserver
+docker push $PROJECT/oortserver/oortserver
 gcloud run deploy oortserver \
   --image $PROJECT/oortserver/oortserver \
   --allow-unauthenticated \
--- scripts/push-docker-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-cd $(realpath $(dirname $0))/..
-PROJECT=us-west1-docker.pkg.dev/oort-319301
-docker tag oort_server:latest $PROJECT/oortserver/oortserver
-docker push $PROJECT/oortserver/oortserver
