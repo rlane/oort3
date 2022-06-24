82420f080f3a02b2fb7c9a7b5d3d2e9aca9b7622
Rich Lane <richlane@google.com>
2022-06-23 18:33:38 -0700

    ai: disable lto

--- scripts/build-ai.sh
+++ scripts/build-ai.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
-export RUSTFLAGS="-C lto=on -C opt-level=s -C link-arg=-zstack-size=1024"
-cargo build --frozen --locked --offline --release --target wasm32-unknown-unknown --message-format=json-diagnostic-short
+export RUSTFLAGS="-C opt-level=s -C link-arg=-zstack-size=1024"
+cargo build -v -j1 --frozen --locked --offline --release --target wasm32-unknown-unknown
