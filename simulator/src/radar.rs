a9ef4914cf1c98baca58304c853aa2fea8691d28
Rich Lane <richlane@google.com>
2022-06-26 12:33:20 -0700

    sim: refactor and optimize radar tick

--- simulator/src/radar.rs
+++ simulator/src/radar.rs
@@ -18,13 +18,26 @@ pub struct Radar {
     pub result: Option<ScanResult>,
 }
 
-struct RadarBeam {
+struct RadarEmitter {
+    handle: ShipHandle,
     center: Point2<f64>,
     width: f64,
     start_bearing: f64,
     end_bearing: f64,
     power: f64,
+    rx_cross_section: f64,
+    min_rssi: f64,
+    classify_rssi: f64,
     center_vec: Vector2<f64>,
+    team: i32,
+}
+
+struct RadarReflector {
+    position: Point2<f64>,
+    velocity: Vector2<f64>,
+    radar_cross_section: f64,
+    team: i32,
+    class: ShipClass,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -44,78 +57,93 @@ pub fn scan(sim: &mut Simulation, own_ship: ShipHandle) -> Option<ScanResult> {
 
 pub fn tick(sim: &mut Simulation) {
     let handle_snapshot: Vec<ShipHandle> = sim.ships.iter().cloned().collect();
-    for own_ship in handle_snapshot {
-        if let Some(radar) = sim.ship_mut(own_ship).data_mut().radar.clone() {
-            let mut result = None;
-            let own_team = sim.ship(own_ship).data().team;
-            let own_position: Point2<f64> = sim.ship(own_ship).position().vector.into();
-            let own_heading = sim.ship(own_ship).heading();
-            let beam = compute_beam(&radar, own_position, own_heading);
-            let mut best_rssi = 0.0;
-            let mut rng = rng::new_rng(sim.tick());
-            for &other in sim.ships.iter() {
-                if sim.ship(other).data().team == own_team {
-                    continue;
-                }
-                let rssi = compute_rssi(sim, &beam, own_ship, other);
-                if rssi > radar.min_rssi && (result.is_none() || rssi > best_rssi) {
-                    result = Some(ScanResult {
-                        class: if rssi > radar.classify_rssi {
-                            Some(sim.ship(other).data().class)
-                        } else {
-                            None
-                        },
-                        position: sim.ship(other).position().vector + noise(&mut rng, rssi),
-                        velocity: sim.ship(other).velocity() + noise(&mut rng, rssi),
-                    });
-                    best_rssi = rssi;
-                }
-            }
-            draw_beam(sim, own_ship, &radar, &beam);
-            sim.ship_mut(own_ship)
-                .data_mut()
-                .radar
-                .as_mut()
-                .unwrap()
-                .result = result;
+
+    let mut emitters: Vec<RadarEmitter> = Vec::new();
+    emitters.reserve(handle_snapshot.len());
+    let mut reflectors: Vec<RadarReflector> = Vec::new();
+    reflectors.reserve(handle_snapshot.len());
+    for handle in handle_snapshot {
+        let ship = sim.ship(handle);
+        let ship_data = ship.data();
+        let position: Point2<f64> = sim.ship(handle).position().vector.into();
+        if let Some(radar) = ship_data.radar.as_ref() {
+            let heading = sim.ship(handle).heading();
+            let h = radar.heading + heading;
+            let w = radar.width;
+            emitters.push(RadarEmitter {
+                handle,
+                team: ship_data.team,
+                center: position,
+                power: radar.power,
+                min_rssi: radar.min_rssi,
+                classify_rssi: radar.classify_rssi,
+                rx_cross_section: radar.rx_cross_section,
+                width: w,
+                start_bearing: h - 0.5 * w,
+                end_bearing: h + 0.5 * w,
+                center_vec: UnitComplex::new(h).transform_vector(&vector![1.0, 0.0]),
+            });
+        }
+
+        {
+            reflectors.push(RadarReflector {
+                team: ship_data.team,
+                position,
+                velocity: sim.ship(handle).velocity(),
+                radar_cross_section: ship_data.radar_cross_section,
+                class: ship_data.class,
+            });
         }
     }
-}
 
-fn compute_beam(radar: &Radar, ship_position: Point2<f64>, ship_heading: f64) -> RadarBeam {
-    let h = radar.heading + ship_heading;
-    let w = radar.width;
-    RadarBeam {
-        center: ship_position,
-        power: radar.power,
-        width: w,
-        start_bearing: h - 0.5 * w,
-        end_bearing: h + 0.5 * w,
-        center_vec: UnitComplex::new(h).transform_vector(&vector![1.0, 0.0]),
+    for emitter in &emitters {
+        let mut result = None;
+        let mut best_rssi = 0.0;
+        let mut rng = rng::new_rng(sim.tick());
+        for reflector in &reflectors {
+            if emitter.team == reflector.team {
+                continue;
+            }
+            let rssi = compute_rssi(emitter, reflector);
+            if rssi > emitter.min_rssi && (result.is_none() || rssi > best_rssi) {
+                result = Some(ScanResult {
+                    class: if rssi > emitter.classify_rssi {
+                        Some(reflector.class)
+                    } else {
+                        None
+                    },
+                    position: reflector.position.coords + noise(&mut rng, rssi),
+                    velocity: reflector.velocity + noise(&mut rng, rssi),
+                });
+                best_rssi = rssi;
+            }
+        }
+        sim.ship_mut(emitter.handle)
+            .data_mut()
+            .radar
+            .as_mut()
+            .unwrap()
+            .result = result;
+    }
+
+    for emitter in &emitters {
+        draw_emitter(sim, emitter);
     }
 }
 
-fn compute_rssi(sim: &Simulation, beam: &RadarBeam, source: ShipHandle, target: ShipHandle) -> f64 {
-    let other_position: Point2<f64> = sim.ship(target).position().vector.into();
-    if (other_position - beam.center).angle(&beam.center_vec) > beam.width * 0.5 {
+fn compute_rssi(emitter: &RadarEmitter, reflector: &RadarReflector) -> f64 {
+    if (reflector.position - emitter.center).angle(&emitter.center_vec) > emitter.width * 0.5 {
         return 0.0;
     }
-    let r_sq = nalgebra::distance_squared(&beam.center, &other_position);
-    let target_cross_section = sim.ship(target).data().radar_cross_section;
-    let rx_cross_section = sim
-        .ship(source)
-        .data()
-        .radar
-        .as_ref()
-        .unwrap()
-        .rx_cross_section;
-    beam.power * target_cross_section * rx_cross_section / (TAU * beam.width * r_sq)
+    let r_sq = nalgebra::distance_squared(&emitter.center, &reflector.position);
+    emitter.power * reflector.radar_cross_section * emitter.rx_cross_section
+        / (TAU * emitter.width * r_sq)
 }
 
-fn compute_approx_range(radar: &Radar, beam: &RadarBeam) -> f64 {
+fn compute_approx_range(emitter: &RadarEmitter) -> f64 {
     let target_cross_section = 5.0;
-    (beam.power * target_cross_section * radar.rx_cross_section
-        / (TAU * beam.width * radar.min_rssi))
+    (emitter.power * target_cross_section * emitter.rx_cross_section
+        / (TAU * emitter.width * emitter.min_rssi))
         .sqrt()
 }
 
@@ -123,17 +151,17 @@ fn noise(rng: &mut SeededRng, rssi: f64) -> Vector2<f64> {
     vector![rng.sample(StandardNormal), rng.sample(StandardNormal)] * (1.0 / rssi)
 }
 
-fn draw_beam(sim: &mut Simulation, ship: ShipHandle, radar: &Radar, beam: &RadarBeam) {
+fn draw_emitter(sim: &mut Simulation, emitter: &RadarEmitter) {
     let color = vector![0.1, 0.2, 0.3, 1.0];
     let mut lines = vec![];
     let n = 20;
-    let w = beam.end_bearing - beam.start_bearing;
-    let center = beam.center;
-    let r = compute_approx_range(radar, beam);
+    let w = emitter.end_bearing - emitter.start_bearing;
+    let center = emitter.center;
+    let r = compute_approx_range(emitter);
     for i in 0..n {
         let frac = (i as f64) / (n as f64);
-        let angle_a = beam.start_bearing + w * frac;
-        let angle_b = beam.start_bearing + w * (frac + 1.0 / n as f64);
+        let angle_a = emitter.start_bearing + w * frac;
+        let angle_b = emitter.start_bearing + w * (frac + 1.0 / n as f64);
         lines.push(Line {
             a: center + vector![r * angle_a.cos(), r * angle_a.sin()],
             b: center + vector![r * angle_b.cos(), r * angle_b.sin()],
@@ -142,13 +170,17 @@ fn draw_beam(sim: &mut Simulation, ship: ShipHandle, radar: &Radar, beam: &Radar
     }
     lines.push(Line {
         a: center,
-        b: center + vector![r * beam.start_bearing.cos(), r * beam.start_bearing.sin()],
+        b: center
+            + vector![
+                r * emitter.start_bearing.cos(),
+                r * emitter.start_bearing.sin()
+            ],
         color,
     });
     lines.push(Line {
         a: center,
-        b: center + vector![r * beam.end_bearing.cos(), r * beam.end_bearing.sin()],
+        b: center + vector![r * emitter.end_bearing.cos(), r * emitter.end_bearing.sin()],
         color,
     });
-    sim.emit_debug_lines(ship, &lines);
+    sim.emit_debug_lines(emitter.handle, &lines);
 }
