98f64297428e6081eaaddd070f9473b95e536791
Rich Lane <richlane@google.com>
2022-06-28 18:16:26 -0700

    worker: install panic handler

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -2,6 +2,7 @@ use oort_worker::SimAgent;
 use yew_agent::PrivateAgent;
 
 fn main() {
+    console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Info).expect("initializing logging");
     log::info!("starting worker");
     SimAgent::register();
