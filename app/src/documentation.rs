dde6bfe81badf929bc2b7af3811468a427f9ecee
Rich Lane <richlane@google.com>
2022-07-02 12:59:56 -0700

    app: update documentation

--- app/src/documentation.rs
+++ app/src/documentation.rs
@@ -10,67 +10,105 @@ pub fn documentation() -> Html {
             <h2>{ "Basics" }</h2>
             { "Select a scenario from the list in the top-right of the page." }<br/>
             { "Press Ctrl-Enter in the editor to run the scenario with a new version of your code." }<br/>
-            { "The game calls your " }<code>{ "tick()" }</code>{ " function 60 times per second." }
 
-            <h2>{ "Ship Control" }</h2>
+            <h2>{ "Controls" }</h2>
             <ul>
-              <li><code>{ "ship.position() → vec2" }</code>{ ": Get the current position in meters." }</li>
-              <li><code>{ "ship.velocity() → vec2" }</code>{ ": Get the current velocity in m/s." }</li>
-              <li><code>{ "ship.heading() → float" }</code>{ ": Get the current heading in radians." }</li>
-              <li><code>{ "ship.angular_velocity() → float" }</code>{ ": Get the current angular velocity in radians/s." }</li>
-              <li><code>{ "ship.accelerate(acceleration: vec2)" }</code>{ ": Linear acceleration. X axis is forward/back, Y axis is left/right. Units are m/s<sup>2</sup>" }</li>
-              <li><code>{ "ship.torque(acceleration: float)" }</code>{ ": Angular acceleration. Unit is radians/s<sup>2</sup>." }</li>
-              <li><code>{ "ship.fire_gun(index=0)" }</code>{ ": Fire a gun." }</li>
-              <li><code>{ "ship.aim_gun(index, angle)" }</code>{ ": Aim a gun." }</li>
-              <li><code>{ "ship.launch_missile(index=0)" }</code>{ ": Launch a missile." }</li>
-              <li><code>{ "ship.class() → string" }</code>{ ": Returns the ship class as a string." }</li>
-              <li><code>{ "ship.explode()" }</code>{ ": Self-destruct." }</li>
+                <li>{ "W/A/S/D: Pan the camera." }</li>
+                <li>{ "Space: Pause/resume." }</li>
+                <li>{ "N: Single-step." }</li>
+                <li>{ "G: Show debug lines for all ships." }</li>
+                <li>{ "Mouse wheel: Zoom." }</li>
+                <li>{ "Mouse click: Select a ship to show debugging info." }</li>
+            </ul>
+
+            <h2>{ "Language" }</h2>
+            <p>
+                { "Oort AIs are written in " }<a href="https://www.rust-lang.org/">{ "Rust" }</a>{ ". " }
+                { "For an introduction to the language check out " }<a href="https://doc.rust-lang.org/stable/rust-by-example/">{ "Rust By Example" }</a>{ ". " }
+                { "Extensive Rust knowledge is "}<em>{ "not" }</em>{ " required to play the game. High school physics and math probably is." }
+            </p>
+
+            <p>
+                { "The starter code for each scenario includes a Ship struct with a "}<code>{ "tick" }</code>{ " method that the game will call 60 times per second. "}
+                { "You can also store state in this struct which can be initialized in "}<code>{ "new" }</code>{ " and accessed with " }<code>{ "self.field_name" }</code>{ ". "}
+            </p>
+
+            <p>
+                { "All interactions between your AI and the game are done using the functions listed below. " }
+                { "Many of these functions take or return "}<code>{ "Vec2" }</code>{ ", which is a 2-dimensional double-precision vector type." }
+            </p>
+
+
+            <h2>{ "Ship Status and Control" }</h2>
+            <ul>
+              <li><code>{ "class() → Class" }</code>{ ": Returns the ship class." }</li>
+              <li><code>{ "position() → Vec2" }</code>{ ": Get the current position in meters." }</li>
+              <li><code>{ "velocity() → Vec2" }</code>{ ": Get the current velocity in m/s." }</li>
+              <li><code>{ "heading() → f64" }</code>{ ": Get the current heading in radians." }</li>
+              <li><code>{ "angular_velocity() → f64" }</code>{ ": Get the current angular velocity in radians/s." }</li>
+              <li><code>{ "accelerate(acceleration: Vec2)" }</code>{ ": Linear acceleration. X axis is forward/back, Y axis is left/right. Units are m/s²" }</li>
+              <li><code>{ "torque(acceleration: f64)" }</code>{ ": Angular acceleration. Unit is radians/s²." }</li>
+            </ul>
+
+            <h2>{ "Weapons" }</h2>
+            <ul>
+              <li><code>{ "fire_gun(index: usize)" }</code>{ ": Fire a gun." }</li>
+              <li><code>{ "aim_gun(index: usize, angle: f64)" }</code>{ ": Aim a gun (for guns on a turret)." }</li>
+              <li><code>{ "launch_missile(index: usize, orders: f64)" }</code>{ ": Launch a missile." }</li>
+              <li><code>{ "explode()" }</code>{ ": Self-destruct." }</li>
             </ul>
 
             <h2>{ "Radar" }</h2>
             <ul>
-              <li><code>{ "radar.set_heading(x)" }</code>{ ": Point the radar at the given heading." }</li>
-              <li><code>{ "radar.set_width(x)" }</code>{ ": Adjust the width of the radar beam (in radians)." }</li>
-              <li><code>{ "radar.scan() → ScanResult" }</code>{ ": Find the closest enemy ship illuminated by the radar." }</li>
-              <li><code>{ "ScanResult { position: vec2, velocity: vec2, found: bool }" }</code></li>
+              <li><code>{ "set_radar_heading(angle: f64)" }</code>{ ": Point the radar at the given heading." }</li>
+              <li><code>{ "set_radar_width(width: f64)" }</code>{ ": Adjust the width of the radar beam (in radians)." }</li>
+              <li><code>{ "scan() → Option<ScanResult>" }</code>{ ": Find the closest enemy ship illuminated by the radar." }</li>
+              <li><code>{ "ScanResult { position: Vec2, velocity: Vec2 }" }</code></li>
             </ul>
 
             <h2>{ "Scalar Math" }</h2>
             <ul>
-              <li><code>{ "x +-*/% y → float" }</code>{ ": Basic arithmetic." }</li>
-              <li><code>{ "-x → float" }</code>{ ": Negation." }</li>
-              <li><code>{ "x ** y → float" }</code>{ ": Exponentiation." }</li>
-              <li><code>{ "abs(x) → float" }</code>{ ": Absolute value." }</li>
-              <li><code>{ "sin(x), cos(x), tan(x) → float" }</code>{ ": Trignometry." }</li>
-              <li><code>{ "sqrt(x) → float" }</code>{ ": Square root." }</li>
-              <li><code>{ "log(x, base) → float" }</code>{ ": Logarithm." }</li>
-              <li><code>{ "min(a, b), max(a, b) → float" }</code>{ ": Minimum and maximum." }</li>
-              <li><code>{ "PI(), E()" }</code>{ ": Constants." }</li>
+              <li><code>{ "PI, TAU" }</code>{ ": Constants."}</li>
+              <li><code>{ "x.abs()" }</code>{ ": Absolute value."}</li>
+              <li><code>{ "x.sqrt()" }</code>{ ": Square root."}</li>
+              <li><code>{ "x.sin(), x.cos(), x.tan()" }</code>{": Trignometry."}</li>
+              <li>{ "See the " }<a href="https://doc.rust-lang.org/std/primitive.f64.html">{ "Rust documentation" }</a>{ " for the full list of f64 methods." }</li>
             </ul>
 
             <h2>{ "Vector Math" }</h2>
             <ul>
-              <li><code>{ "vec2(x, y) → vec2" }</code>{ ": Create a vector." }</li>
-              <li><code>{ "v.x, v.y → float" }</code>{ ": Get a component of a vector." }</li>
-              <li><code>{ "v1 +- v2 → vec2" }</code>{ ": Basic arithmetic between vectors." }</li>
-              <li><code>{ "v */ float → vec2" }</code>{ ": Basic arithmetic between vectors and scalars." }</li>
-              <li><code>{ "-v → vec2" }</code>{ ": Negate a vector." }</li>
-              <li><code>{ "v.magnitude() → float" }</code>{ ": Magnitude (length)." }</li>
-              <li><code>{ "v.normalize() → vec2" }</code>{ ": Normalize to a unit vector." }</li>
-              <li><code>{ "v.rotate(float) → vec2" }</code>{ ": Rotate counter-clockwise." }</li>
-              <li><code>{ "v.angle() → float" }</code>{ ": Angle of a vector." }</li>
-              <li><code>{ "v1.dot(v2) → float" }</code>{ ": Dot product." }</li>
-              <li><code>{ "v1.distance(v2) → float" }</code>{ ": Distance between two points." }</li>
+              <li><code>{ "vec2(x, y) → Vec2" }</code>{ ": Create a vector." }</li>
+              <li><code>{ "v.x, v.y → f64" }</code>{ ": Get a component of a vector." }</li>
+              <li><code>{ "v1 +- v2 → Vec2" }</code>{ ": Basic arithmetic between vectors." }</li>
+              <li><code>{ "v */ f64 → Vec2" }</code>{ ": Basic arithmetic between vectors and scalars." }</li>
+              <li><code>{ "-v → Vec2" }</code>{ ": Negate a vector." }</li>
+              <li><code>{ "v.length() → f64" }</code>{ ": Length." }</li>
+              <li><code>{ "v.normalize() → Vec2" }</code>{ ": Normalize to a unit vector." }</li>
+              <li><code>{ "v.rotate(f64) → Vec2" }</code>{ ": Rotate counter-clockwise." }</li>
+              <li><code>{ "v.angle() → f64" }</code>{ ": Angle of a vector." }</li>
+              <li><code>{ "v1.dot(v2: Vec2) → f64" }</code>{ ": Dot product." }</li>
+              <li><code>{ "v1.distance(v2: Vec2) → f64" }</code>{ ": Distance between two points." }</li>
             </ul>
 
             <h2>{ "Miscellaneous" }</h2>
             <ul>
-              <li><code>{ "print(string)" }</code>{ ": Log a message to the browser console." }</li>
-              <li><code>{ "rng.next(low, high) → float" }</code>{ ": Get a random number." }</li>
               <li><code>{ "angle_diff(a, b) → float" }</code>{ ": Returns the shortest (possibly negative) distance between two angles." }</li>
-              <li><code>{ "dbg.line(v0, v1, color: int)" }</code>{ ": Draw a line. Color is 24-bit RGB." }</li>
-              <li><a href="https://rhai.rs/book/language/index.html" target="_blank">{ "Rhai Language Reference" }</a></li>
+              <li>{ "Not yet implemented: "}<code>{ "rand(low, high) → float" }</code>{ ": Get a random number." }</li>
+              <li><code>{ "debug_text(&str)" }</code>{ ": Add text to be displayed when the ship is selected by clicking on it." }</li>
+              <li>{ "Not yet implemented: "}<code>{ "debug_line(v0, v1, color: int)" }</code>{ ": Draw a line. Color is 24-bit RGB." }</li>
+              <li><code>{ "orders() → f64" }</code>{ ": Returns the orders passed to "}<code>{ "launch_missile" }</code>{ "." }</li>
+              <li><code>{ "seed() → u128" }</code>{ ": Returns a seed useful for initializing a random number generator." }</li>
             </ul>
+
+            <h2>{ "Ship Classes" }</h2>
+            <ul>
+              <li>{ "Fighter: Small, fast, and lightly armored. One forward-facing gun and one missile launcher. "}</li>
+              <li>{ "Frigate: Medium size with heavy armor. One forward-facing high-velocity gun, two turreted guns, and one missile launcher. "}</li>
+              <li>{ "Cruiser: Large, slow, and heavily armored. One turreted flak gun, two missile launchers, and one torpedo launcher. "}</li>
+              <li>{ "Missile: Highly maneuverable but unarmored. Explodes on contact or after an " }<code>{ "explode()" }</code>{ " call." }</li>
+              <li>{ "Torpedo: Better armor, larger warhead, but less maneuverable than a missile. Explodes on contact or after an " }<code>{ "explode()" }</code>{ " call." }</li>
+            </ul>
+
             <h2>{ "Credits" }</h2>
             { "Rich Lane" }<br/>
             <br/>
