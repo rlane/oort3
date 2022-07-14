026068433d3dc4dea30323bcb349e870f73caa94
Rich Lane <richlane@google.com>
2022-07-13 21:39:59 -0700

    sim: add a fuzz test

---
+++ ai/fuzz.rs
@@ -0,0 +1,37 @@
+use crate::prelude::*;
+
+pub struct Ship {}
+
+impl Ship {
+    pub fn new() -> Ship {
+        Ship {}
+    }
+
+    pub fn tick(&mut self) {
+        debug_triangle(vec2(gen(), gen()), gen(), 0xffffff);
+        for i in 0..(SystemState::MaxSize as u8) {
+            if i == SystemState::Explode as u8 {
+                continue;
+            }
+            crate::sys::write_system_state(unsafe { std::mem::transmute(i) }, gen());
+        }
+    }
+}
+
+fn gen() -> f64 {
+    let r = rand(0.0, 1.0);
+    if r < 0.1 {
+        let vals = &[
+            f64::NAN,
+            f64::INFINITY,
+            f64::NEG_INFINITY,
+            f64::MIN,
+            f64::MIN_POSITIVE,
+            f64::MAX,
+            1.0e-308_f64,
+        ];
+        vals[rand(0.0, vals.len() as f64) as usize]
+    } else {
+        rand(f64::MIN, f64::MAX)
+    }
+}
---
+++ simulator/tests/fuzz_test.rs
@@ -0,0 +1,20 @@
+use oort_simulator::scenario;
+use oort_simulator::simulation;
+use rayon::prelude::*;
+use test_log::test;
+
+#[test]
+fn test_fuzz() {
+    let scenario_name = "fleet";
+    let scenario = scenario::load(scenario_name);
+    let mut codes = scenario.initial_code();
+    codes[0] = simulation::Code::Builtin("fuzz".to_string());
+    (0..10u32).into_par_iter().for_each(|seed| {
+        let mut sim = simulation::Simulation::new(scenario_name, seed, &codes);
+        let mut i = 0;
+        while sim.status() == scenario::Status::Running && i < 200 {
+            sim.step();
+            i += 1;
+        }
+    });
+}
