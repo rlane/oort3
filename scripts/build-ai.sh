6bb7c76044e6e0b1cf711f7f770e6eeb92d3a71d
Rich Lane <richlane@google.com>
2022-08-10 05:58:29 -0700

    remove --frozen and --locked from build-ai.sh

--- scripts/build-ai.sh
+++ scripts/build-ai.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
 export RUSTFLAGS="-C opt-level=s -C link-arg=-zstack-size=16384"
-cargo build -p oort_ai -v -j1 --frozen --locked --offline --release --target wasm32-unknown-unknown
+cargo build -p oort_ai -v -j1 --offline --release --target wasm32-unknown-unknown
