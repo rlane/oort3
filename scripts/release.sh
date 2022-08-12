225c0ffb577e8ba4af2f2f45719bd0abf4aa9ebe
Rich Lane <richlane@google.com>
2022-08-12 14:23:18 -0700

    release: do not create tags per crate

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
-cargo workspaces publish --all
+cargo workspaces publish --all --no-individual-tags
 scripts/build-docker-image.sh
 scripts/deploy-oort-server.sh
 scripts/push.sh
