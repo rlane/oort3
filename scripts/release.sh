ddaa38ab5bcf60b873a90bd8b4aa2c720faa5ae6
Rich Lane <richlane@google.com>
2022-08-12 14:17:33 -0700

    also bump verison for private crates

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
-cargo workspaces publish
+cargo workspaces publish --all
 scripts/build-docker-image.sh
 scripts/deploy-oort-server.sh
 scripts/push.sh
