017872e2b397b1a8ea9624297fc6dc75ccfa343c
Rich Lane <richlane@google.com>
2022-09-05 21:21:50 -0700

    scripts: bump all crate versions in release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
-cargo workspaces publish --all --no-individual-tags
+cargo workspaces publish --all --no-individual-tags --force='*'
 scripts/build-docker-image.sh
 scripts/deploy-oort-server.sh
 scripts/push.sh
