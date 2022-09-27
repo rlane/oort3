edb86401124d7c4f57b82a56cadc115769e80c7a
Rich Lane <richlane@google.com>
2022-09-27 08:12:58 -0700

    scripts: add options to release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,24 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
+
+PUSH_ALL=1
+PUSH_APP=0
+PUSH_SERVER=0
+while getopts "ws" option; do
+   case $option in
+      w) PUSH_ALL=0; PUSH_APP=1;;
+      s) PUSH_ALL=0; PUSH_SERVER=1;;
+      \?) exit;;
+   esac
+done
+
 cargo workspaces publish --all --no-individual-tags --force='*'
-scripts/build-docker-image.sh
-scripts/deploy-oort-server.sh
-scripts/push.sh
+
+if [[ $PUSH_ALL -eq 1 || $PUSH_SERVER  -eq 1 ]]; then
+  scripts/build-docker-image.sh
+  scripts/deploy-oort-server.sh
+fi
+
+if [[ $PUSH_ALL -eq 1 || $PUSH_APP -eq 1 ]]; then
+  scripts/push.sh
+fi
