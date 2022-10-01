63eb7a14b02492bc76edca912636e335075525a2
Rich Lane <richlane@google.com>
2022-10-01 12:13:52 -0700

    app: change telemetry service URL

--- app/js/telemetry.js
+++ app/js/telemetry.js
@@ -4,7 +4,7 @@ export function send_telemetry(data) {
     return;
   }
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://us-central1-oort-319301.cloudfunctions.net/upload");
+  xhr.open("POST", "https://telemetry.oort.rs/post");
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhr.send(data);
   console.log("Sent telemetry: " + data);
