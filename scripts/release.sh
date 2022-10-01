0822d6d59eaaee42c591129f0d3152b199a0b45e
Rich Lane <richlane@google.com>
2022-10-01 12:12:18 -0700

    scripts: add telemetry service to release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,10 +4,12 @@ cd $(realpath $(dirname $0))/..
 PUSH_ALL=1
 PUSH_APP=0
 PUSH_COMPILER_SERVICE=0
+PUSH_TELEMETRY_SERVICE=0
 while getopts "ws" option; do
    case $option in
       w) PUSH_ALL=0; PUSH_APP=1;;
       c) PUSH_ALL=0; PUSH_COMPILER_SERVICE=1;;
+      t) PUSH_ALL=0; PUSH_TELEMETRY_SERVICE=1;;
       \?) exit;;
    esac
 done
@@ -17,6 +19,11 @@ cargo workspaces publish --all --no-individual-tags --force='*'
 if [[ $PUSH_ALL -eq 1 || $PUSH_COMPILER_SERVICE  -eq 1 ]]; then
   scripts/build-compiler-service-docker-image.sh
   scripts/deploy-compiler-service.sh
+fi
+
+if [[ $PUSH_ALL -eq 1 || $PUSH_TELEMETRY_SERVICE  -eq 1 ]]; then
+  scripts/build-telemetry-service-docker-image.sh
+  scripts/deploy-telemetry-service.sh
 fi
 
 if [[ $PUSH_ALL -eq 1 || $PUSH_APP -eq 1 ]]; then
