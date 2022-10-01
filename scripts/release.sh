6be52e23b2ec9e71015f6cb178bbef8828ea8e8d
Rich Lane <richlane@google.com>
2022-10-01 12:29:49 -0700

    scripts: make bumping version optional in release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,20 +1,35 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 cd $(realpath $(dirname $0))/..
 
+function usage() {
+  echo "Usage: $0 -[wctsh]"
+  echo
+  echo "  -w: Push app"
+  echo "  -c: Push compiler service"
+  echo "  -t: Push telemetry service"
+  echo "  -s: Skip bumping version"
+  echo "  -h: Display this message"
+}
+
+BUMP_VERSION=1
 PUSH_ALL=1
 PUSH_APP=0
 PUSH_COMPILER_SERVICE=0
 PUSH_TELEMETRY_SERVICE=0
-while getopts "ws" option; do
+while getopts "wctsh" option; do
    case $option in
       w) PUSH_ALL=0; PUSH_APP=1;;
       c) PUSH_ALL=0; PUSH_COMPILER_SERVICE=1;;
       t) PUSH_ALL=0; PUSH_TELEMETRY_SERVICE=1;;
-      \?) exit;;
+      s) BUMP_VERSION=0;;
+      h) usage; exit;;
+      \?) usage; exit;;
    esac
 done
 
-cargo workspaces publish --all --no-individual-tags --force='*'
+if [[ $BUMP_VERSION -eq 1 ]]; then
+  cargo workspaces publish --all --no-individual-tags --force='*'
+fi
 
 if [[ $PUSH_ALL -eq 1 || $PUSH_COMPILER_SERVICE  -eq 1 ]]; then
   scripts/build-compiler-service-docker-image.sh
