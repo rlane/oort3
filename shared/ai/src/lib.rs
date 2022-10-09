96bd4bd0114cb4af6b26f1126b186e50cf7c8be6
Rich Lane <richlane@google.com>
2022-10-08 21:43:51 -0700

    sim: use one VM per team

--- shared/ai/src/lib.rs
+++ shared/ai/src/lib.rs
@@ -3,17 +3,33 @@ mod user;
 // For compatibility.
 pub use oort_api::prelude;
 
-static mut USER_STATE: Option<user::Ship> = None;
+static mut SHIPS: Vec<Option<(usize, user::Ship)>> = Vec::new();
 
 #[doc(hidden)]
 #[no_mangle]
-pub fn export_tick() {
+pub fn export_tick_ship(index: u32, generation: u32) {
+    let index = index as usize;
+    let generation = generation as usize;
     unsafe {
         oort_api::dbg::reset();
-        if USER_STATE.is_none() {
-            USER_STATE = Some(user::Ship::new());
+        if SHIPS.len() <= index {
+            SHIPS.resize_with(index + 1, || None);
         }
-        USER_STATE.as_mut().unwrap().tick();
+        match SHIPS.get_mut(index) {
+            Some(Some((gen, ship))) => {
+                if *gen != generation {
+                    *ship = user::Ship::new();
+                    *gen = generation;
+                }
+                ship.tick();
+            }
+            Some(x @ None) => {
+                let mut ship = user::Ship::new();
+                ship.tick();
+                *x = Some((generation, ship));
+            }
+            _ => {}
+        }
         oort_api::dbg::update();
     }
 }
--- shared/simulator/src/vm/mod.rs
+++ shared/simulator/src/vm/mod.rs
@@ -61,7 +61,21 @@ pub fn new_team_controller(code: &Code) -> Result<Box<dyn TeamController>, Error
 }
 
 pub struct WasmTeamController {
-    pub module: Module,
+    shared: WasmShared,
+}
+
+struct WasmShipController {
+    sim: *mut Simulation,
+    handle: ShipHandle,
+    shared: WasmShared,
+    state: LocalSystemState,
+}
+
+#[derive(Clone)]
+pub struct WasmShared {
+    memory: wasmer::Memory,
+    system_state_ptr: WasmPtr<f64>,
+    tick_ship: wasmer::Function,
 }
 
 impl WasmTeamController {
@@ -74,19 +88,8 @@ impl WasmTeamController {
         #[cfg(target_arch = "wasm32")]
         let store = Store::default();
         let module = translate_error(Module::new(&store, code))?;
-
-        Ok(Box::new(WasmTeamController { module }))
-    }
-}
-
-impl TeamController for WasmTeamController {
-    fn create_ship_controller(
-        &mut self,
-        handle: ShipHandle,
-        sim: &mut Simulation,
-    ) -> Result<Box<dyn ShipController>, Error> {
         let import_object = imports! {};
-        let instance = Instance::new(&self.module, &import_object)?;
+        let instance = Instance::new(&module, &import_object)?;
 
         let memory = translate_error(instance.exports.get_memory("memory"))?.clone();
         let system_state_offset: i32 =
@@ -96,65 +99,76 @@ impl TeamController for WasmTeamController {
                 .unwrap();
         let system_state_ptr: WasmPtr<f64> = WasmPtr::new(system_state_offset as u32);
 
-        let tick = translate_error(instance.exports.get_function("export_tick"))?.clone();
+        let tick_ship = translate_error(instance.exports.get_function("export_tick_ship"))?.clone();
 
-        let ctrl = WasmShipController {
-            handle,
-            sim,
+        let shared = WasmShared {
             memory,
             system_state_ptr,
-            tick,
+            tick_ship,
         };
 
-        let mut state = ctrl.read_system_state();
-        state.set(
+        Ok(Box::new(WasmTeamController { shared }))
+    }
+}
+
+impl TeamController for WasmTeamController {
+    fn create_ship_controller(
+        &mut self,
+        handle: ShipHandle,
+        sim: &mut Simulation,
+    ) -> Result<Box<dyn ShipController>, Error> {
+        let mut ctrl = WasmShipController {
+            handle,
+            sim,
+            shared: self.shared.clone(),
+            state: LocalSystemState::new(),
+        };
+
+        ctrl.state.set(
             SystemState::Seed,
             (make_seed(sim.seed(), handle) & 0xffffff) as f64,
         );
         if let Some(radar) = sim.ship(handle).data().radar.as_ref() {
-            state.set(SystemState::RadarHeading, radar.heading);
-            state.set(SystemState::RadarWidth, radar.width);
-            state.set(SystemState::RadarMinDistance, radar.min_distance);
-            state.set(SystemState::RadarMaxDistance, radar.max_distance);
+            ctrl.state.set(SystemState::RadarHeading, radar.heading);
+            ctrl.state.set(SystemState::RadarWidth, radar.width);
+            ctrl.state
+                .set(SystemState::RadarMinDistance, radar.min_distance);
+            ctrl.state
+                .set(SystemState::RadarMaxDistance, radar.max_distance);
         }
-        ctrl.write_system_state(&state);
 
         Ok(Box::new(ctrl))
     }
 }
 
-struct WasmShipController {
-    pub handle: ShipHandle,
-    pub sim: *mut Simulation,
-    pub memory: wasmer::Memory,
-    pub system_state_ptr: WasmPtr<f64>,
-    pub tick: wasmer::Function,
-}
-
 impl WasmShipController {
-    pub fn read_system_state(&self) -> LocalSystemState {
-        let mut state = [0.0; SystemState::Size as usize];
+    pub fn read_system_state(&mut self) {
         let slice = self
+            .shared
             .system_state_ptr
-            .slice(&self.memory, SystemState::Size as u32)
+            .slice(&self.shared.memory, SystemState::Size as u32)
             .expect("system state read");
-        slice.read_slice(&mut state).expect("system state read");
-        LocalSystemState { state }
+        slice
+            .read_slice(&mut self.state.state)
+            .expect("system state read");
     }
 
-    pub fn write_system_state(&self, state: &LocalSystemState) {
+    pub fn write_system_state(&self) {
         let slice = self
+            .shared
             .system_state_ptr
-            .slice(&self.memory, SystemState::Size as u32)
+            .slice(&self.shared.memory, SystemState::Size as u32)
             .expect("system state write");
-        slice.write_slice(&state.state).expect("system state write");
+        slice
+            .write_slice(&self.state.state)
+            .expect("system state write");
     }
 
     pub fn read_string(&self, offset: u32, length: u32) -> Option<String> {
         let ptr: WasmPtr<u8> = WasmPtr::new(offset);
         let mut bytes: Vec<u8> = Vec::new();
         bytes.resize(length as usize, 0);
-        let slice = ptr.slice(&self.memory, length).ok()?;
+        let slice = ptr.slice(&self.shared.memory, length).ok()?;
         slice.read_slice(&mut bytes).ok()?;
         String::from_utf8(bytes).ok()
     }
@@ -162,7 +176,7 @@ impl WasmShipController {
     pub fn read_vec<T: Default + Clone>(&self, offset: u32, length: u32) -> Option<Vec<T>> {
         let ptr: WasmPtr<u8> = WasmPtr::new(offset);
         let byte_length = length.saturating_mul(std::mem::size_of::<T>() as u32);
-        let slice = ptr.slice(&self.memory, byte_length).ok()?;
+        let slice = ptr.slice(&self.shared.memory, byte_length).ok()?;
         let byte_vec = slice.read_to_vec().ok()?;
         let src_ptr = unsafe { std::mem::transmute::<*const u8, *const T>(byte_vec.as_ptr()) };
         let src_slice = unsafe { std::slice::from_raw_parts(src_ptr, length as usize) };
@@ -184,91 +198,107 @@ fn make_seed(sim_seed: u32, handle: ShipHandle) -> i64 {
 impl ShipController for WasmShipController {
     fn tick(&mut self) -> Result<(), Error> {
         {
-            let mut state = self.read_system_state();
             let sim = unsafe { &mut *self.sim };
 
-            state.set(
+            self.state.set(
                 SystemState::Class,
                 translate_class(sim.ship(self.handle).data().class) as u32 as f64,
             );
 
             let position = sim.ship(self.handle).position();
-            state.set(SystemState::PositionX, position.x);
-            state.set(SystemState::PositionY, position.y);
+            self.state.set(SystemState::PositionX, position.x);
+            self.state.set(SystemState::PositionY, position.y);
 
             let velocity = sim.ship(self.handle).velocity();
-            state.set(SystemState::VelocityX, velocity.x);
-            state.set(SystemState::VelocityY, velocity.y);
+            self.state.set(SystemState::VelocityX, velocity.x);
+            self.state.set(SystemState::VelocityY, velocity.y);
 
-            state.set(SystemState::Heading, sim.ship(self.handle).heading());
-            state.set(
+            self.state
+                .set(SystemState::Heading, sim.ship(self.handle).heading());
+            self.state.set(
                 SystemState::AngularVelocity,
                 sim.ship(self.handle).angular_velocity(),
             );
 
             if let Some(radar) = sim.ship_mut(self.handle).data_mut().radar.as_mut() {
-                state.set(SystemState::RadarHeading, radar.get_heading());
-                state.set(SystemState::RadarWidth, radar.get_width());
-                state.set(SystemState::RadarMinDistance, radar.get_min_distance());
-                state.set(SystemState::RadarMaxDistance, radar.get_max_distance());
+                self.state
+                    .set(SystemState::RadarHeading, radar.get_heading());
+                self.state.set(SystemState::RadarWidth, radar.get_width());
+                self.state
+                    .set(SystemState::RadarMinDistance, radar.get_min_distance());
+                self.state
+                    .set(SystemState::RadarMaxDistance, radar.get_max_distance());
 
                 if let Some(contact) = radar.scan() {
-                    state.set(SystemState::RadarContactFound, 1.0);
-                    state.set(SystemState::RadarContactPositionX, contact.position.x);
-                    state.set(SystemState::RadarContactPositionY, contact.position.y);
-                    state.set(SystemState::RadarContactVelocityX, contact.velocity.x);
-                    state.set(SystemState::RadarContactVelocityY, contact.velocity.y);
-                    state.set(
+                    self.state.set(SystemState::RadarContactFound, 1.0);
+                    self.state
+                        .set(SystemState::RadarContactPositionX, contact.position.x);
+                    self.state
+                        .set(SystemState::RadarContactPositionY, contact.position.y);
+                    self.state
+                        .set(SystemState::RadarContactVelocityX, contact.velocity.x);
+                    self.state
+                        .set(SystemState::RadarContactVelocityY, contact.velocity.y);
+                    self.state.set(
                         SystemState::RadarContactClass,
                         translate_class(contact.class) as u32 as f64,
                     );
                 } else {
-                    state.set(SystemState::RadarContactFound, 0.0);
+                    self.state.set(SystemState::RadarContactFound, 0.0);
                 }
             }
 
             {
                 let ship = sim.ship(self.handle);
                 let data = ship.data();
-                state.set(SystemState::MaxAccelerationX, data.max_acceleration.x);
-                state.set(SystemState::MaxAccelerationY, data.max_acceleration.y);
-                state.set(
+                self.state
+                    .set(SystemState::MaxAccelerationX, data.max_acceleration.x);
+                self.state
+                    .set(SystemState::MaxAccelerationY, data.max_acceleration.y);
+                self.state.set(
                     SystemState::MaxAngularAcceleration,
                     data.max_angular_acceleration,
                 );
             }
 
             if let Some(radio) = sim.ship(self.handle).data().radio.as_ref() {
-                state.set(SystemState::RadioChannel, radio.get_channel() as f64);
-                state.set(
+                self.state
+                    .set(SystemState::RadioChannel, radio.get_channel() as f64);
+                self.state.set(
                     SystemState::RadioReceive,
                     radio.get_received().unwrap_or(0.0),
                 );
-                state.set(SystemState::RadioSend, 0.0);
+                self.state.set(SystemState::RadioSend, 0.0);
             }
 
-            state.set(SystemState::CurrentTick, sim.tick() as f64);
-            state.set(SystemState::Energy, sim.ship(self.handle).data().energy);
+            self.state.set(SystemState::CurrentTick, sim.tick() as f64);
+            self.state
+                .set(SystemState::Energy, sim.ship(self.handle).data().energy);
 
-            self.write_system_state(&state);
+            self.write_system_state();
         }
 
-        translate_error(self.tick.call(&[]))?;
+        let (index, generation) = self.handle.0.into_raw_parts();
+        translate_error(
+            self.shared
+                .tick_ship
+                .call(&[index.into(), generation.into()]),
+        )?;
 
         {
-            let mut state = self.read_system_state();
+            self.read_system_state();
             let sim = unsafe { &mut *self.sim };
 
             sim.ship_mut(self.handle).accelerate(Vec2::new(
-                state.get(SystemState::AccelerateX),
-                state.get(SystemState::AccelerateY),
+                self.state.get(SystemState::AccelerateX),
+                self.state.get(SystemState::AccelerateY),
             ));
-            state.set(SystemState::AccelerateX, 0.0);
-            state.set(SystemState::AccelerateY, 0.0);
+            self.state.set(SystemState::AccelerateX, 0.0);
+            self.state.set(SystemState::AccelerateY, 0.0);
 
             sim.ship_mut(self.handle)
-                .torque(state.get(SystemState::Torque));
-            state.set(SystemState::Torque, 0.0);
+                .torque(self.state.get(SystemState::Torque));
+            self.state.set(SystemState::Torque, 0.0);
 
             for (i, (aim, fire)) in [
                 (SystemState::Aim0, SystemState::Fire0),
@@ -279,36 +309,37 @@ impl ShipController for WasmShipController {
             .iter()
             .enumerate()
             {
-                if state.get(*fire) > 0.0 {
-                    sim.ship_mut(self.handle).aim(i as i64, state.get(*aim));
+                if self.state.get(*fire) > 0.0 {
+                    sim.ship_mut(self.handle)
+                        .aim(i as i64, self.state.get(*aim));
                     sim.ship_mut(self.handle).fire(i as i64);
-                    state.set(*fire, 0.0);
+                    self.state.set(*fire, 0.0);
                 }
             }
 
             if let Some(radar) = sim.ship_mut(self.handle).data_mut().radar.as_mut() {
-                radar.set_heading(state.get(SystemState::RadarHeading));
-                radar.set_width(state.get(SystemState::RadarWidth));
-                radar.set_min_distance(state.get(SystemState::RadarMinDistance));
-                radar.set_max_distance(state.get(SystemState::RadarMaxDistance));
+                radar.set_heading(self.state.get(SystemState::RadarHeading));
+                radar.set_width(self.state.get(SystemState::RadarWidth));
+                radar.set_min_distance(self.state.get(SystemState::RadarMinDistance));
+                radar.set_max_distance(self.state.get(SystemState::RadarMaxDistance));
             }
 
-            if state.get(SystemState::Explode) > 0.0 {
+            if self.state.get(SystemState::Explode) > 0.0 {
                 sim.ship_mut(self.handle).explode();
-                state.set(SystemState::Explode, 0.0);
+                self.state.set(SystemState::Explode, 0.0);
             }
 
-            if state.get(SystemState::DebugTextLength) > 0.0 {
-                let offset = state.get(SystemState::DebugTextPointer) as u32;
-                let length = state.get(SystemState::DebugTextLength) as u32;
+            if self.state.get(SystemState::DebugTextLength) > 0.0 {
+                let offset = self.state.get(SystemState::DebugTextPointer) as u32;
+                let length = self.state.get(SystemState::DebugTextLength) as u32;
                 if let Some(s) = self.read_string(offset, length) {
                     sim.emit_debug_text(self.handle, s);
                 }
             }
 
-            if state.get(SystemState::DebugLinesLength) > 0.0 {
-                let offset = state.get(SystemState::DebugLinesPointer) as u32;
-                let length = state.get(SystemState::DebugLinesLength) as u32;
+            if self.state.get(SystemState::DebugLinesLength) > 0.0 {
+                let offset = self.state.get(SystemState::DebugLinesPointer) as u32;
+                let length = self.state.get(SystemState::DebugLinesLength) as u32;
                 if length <= 128 {
                     if let Some(lines) = self.read_vec::<Line>(offset, length) {
                         sim.emit_debug_lines(
@@ -327,23 +358,19 @@ impl ShipController for WasmShipController {
             }
 
             if let Some(radio) = sim.ship_mut(self.handle).data_mut().radio.as_mut() {
-                radio.set_channel(state.get(SystemState::RadioChannel) as usize);
-                let data = state.get(SystemState::RadioSend);
+                radio.set_channel(self.state.get(SystemState::RadioChannel) as usize);
+                let data = self.state.get(SystemState::RadioSend);
                 if data != 0.0 {
                     radio.set_sent(Some(data));
                 }
             }
-
-            self.write_system_state(&state);
         }
         Ok(())
     }
 
     fn write_target(&mut self, target: Vec2) {
-        let mut state = self.read_system_state();
-        state.set(SystemState::RadarContactPositionX, target.x);
-        state.set(SystemState::RadarContactPositionY, target.y);
-        self.write_system_state(&state);
+        self.state.set(SystemState::RadarContactPositionX, target.x);
+        self.state.set(SystemState::RadarContactPositionY, target.y);
     }
 }
 
@@ -352,6 +379,12 @@ struct LocalSystemState {
 }
 
 impl LocalSystemState {
+    pub fn new() -> Self {
+        Self {
+            state: [0.0; SystemState::Size as usize],
+        }
+    }
+
     pub fn get(&self, index: SystemState) -> f64 {
         let v = self.state[index as usize];
         if v.is_nan() || v.is_infinite() {
