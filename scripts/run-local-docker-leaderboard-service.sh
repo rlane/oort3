4f9b81ee67d5c581e5d661f4f6d5c9417310b2d9
Rich Lane <richlane@google.com>
2022-10-14 23:17:35 -0700

    scripts: add --rm to docker command lines

--- scripts/run-local-docker-leaderboard-service.sh
+++ scripts/run-local-docker-leaderboard-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eux
-docker run -it -p 127.0.0.1:8083:8080/tcp --env RUST_LOG=debug --env ENVIRONMENT=dev oort_leaderboard_service
+docker run --rm -it -p 127.0.0.1:8083:8080/tcp --env RUST_LOG=debug --env ENVIRONMENT=dev oort_leaderboard_service
--- scripts/run-local-docker-telemetry-service.sh
+++ scripts/run-local-docker-telemetry-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eux
-docker run -it -p 127.0.0.1:8082:8080/tcp --env RUST_LOG=debug --env ENVIRONMENT=dev oort_telemetry_service
+exec docker run --rm -it -p 127.0.0.1:8082:8080/tcp --env RUST_LOG=debug --env ENVIRONMENT=dev oort_telemetry_service
