3257515f22f7aa124736c34fcc9221a4b344e3b2
Rich Lane <richlane@google.com>
2022-10-15 09:52:59 -0700

    compiler_service: add format handler

--- services/compiler/src/main.rs
+++ services/compiler/src/main.rs
@@ -37,6 +37,41 @@ async fn compile(req: &mut Request, res: &mut Response) {
     }
 }
 
+async fn format_internal(req: &mut Request, res: &mut Response) -> anyhow::Result<()> {
+    log::debug!("Got request {:?}", req);
+    let payload = req.payload().await?;
+    let code = std::str::from_utf8(payload)?;
+    log::debug!("Code: {}", code);
+    let filename = "ai/src/format.rs";
+    std::fs::write(filename, payload)?;
+    let start_time = std::time::Instant::now();
+    let output = Command::new("rustfmt").args([filename]).output().await?;
+    let elapsed = std::time::Instant::now() - start_time;
+    if !output.status.success() {
+        log::info!("Format failed in {:?}", elapsed);
+        res.set_status_code(StatusCode::BAD_REQUEST);
+        let stdout = std::str::from_utf8(&output.stdout)?;
+        let stderr = std::str::from_utf8(&output.stderr)?;
+        log::debug!("Format failed: stderr={}\nstdout={}", stderr, stdout);
+        res.render(stderr.to_string());
+        return Ok(());
+    }
+    log::info!("Format succeeded in {:?}", elapsed);
+    log::debug!("Format finished: {}", std::str::from_utf8(&output.stderr)?);
+    let formatted = std::fs::read(filename)?;
+    res.write_body(Bytes::copy_from_slice(&formatted))?;
+    Ok(())
+}
+
+#[handler]
+async fn format(req: &mut Request, res: &mut Response) {
+    if let Err(e) = format_internal(req, res).await {
+        log::error!("Format request error: {}", e);
+        res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
+        res.render(e.to_string());
+    }
+}
+
 #[handler]
 async fn nop(_req: &mut Request, res: &mut Response) {
     res.render("");
@@ -66,7 +101,8 @@ async fn main() {
         .build();
 
     let router = Router::with_hoop(cors_handler)
-        .push(Router::with_path("/compile").post(compile).options(nop));
+        .push(Router::with_path("/compile").post(compile).options(nop))
+        .push(Router::with_path("/format").post(format).options(nop));
     log::info!("Starting oort_compiler_service v1");
     Server::new(TcpListener::bind(&format!("0.0.0.0:{}", port)))
         .serve(router)
