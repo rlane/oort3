46f186d4f4e6949e0f89733bc639ea1d21a5256a
Rich Lane <richlane@google.com>
2022-11-20 14:57:21 -0800

    sim: add missile_duel scenario

--- shared/simulator/src/scenario.rs
+++ shared/simulator/src/scenario.rs
@@ -95,6 +95,12 @@ fn fighter_without_missiles_or_radar(team: i32) -> ShipData {
     data
 }
 
+fn fighter_without_guns(team: i32) -> ShipData {
+    let mut data = fighter(team);
+    data.guns.pop();
+    data
+}
+
 fn target_asteroid(variant: i32) -> ShipData {
     let mut asteroid = asteroid(variant);
     asteroid.team = 1;
@@ -195,6 +201,7 @@ pub fn load_safe(name: &str) -> Option<Box<dyn Scenario>> {
         // Tournament
         "primitive_duel" => Some(Box::new(PrimitiveDuel::new())),
         "fighter_duel" => Some(Box::new(FighterDuel::new())),
+        "missile_duel" => Some(Box::new(MissileDuel::new())),
         "frigate_duel" => Some(Box::new(FrigateDuel::new())),
         "cruiser_duel" => Some(Box::new(CruiserDuel::new())),
         "asteroid_duel" => Some(Box::new(AsteroidDuel::new())),
@@ -232,6 +239,7 @@ pub fn list() -> Vec<String> {
         "tutorial11",
         "gunnery",
         "fighter_duel",
+        "missile_duel",
         "frigate_duel",
         "cruiser_duel",
         "asteroid_duel",
@@ -1455,6 +1463,64 @@ impl Scenario for FighterDuel {
             vector![0.0, 0.0],
             std::f64::consts::PI,
             fighter(1),
+        );
+    }
+
+    fn status(&self, sim: &Simulation) -> Status {
+        check_tournament_victory(sim)
+    }
+
+    fn initial_code(&self) -> Vec<Code> {
+        vec![empty_ai(), reference_ai()]
+    }
+
+    fn solution(&self) -> Code {
+        reference_ai()
+    }
+
+    fn is_tournament(&self) -> bool {
+        true
+    }
+}
+
+struct MissileDuel {}
+
+impl MissileDuel {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Scenario for MissileDuel {
+    fn name(&self) -> String {
+        "missile_duel".into()
+    }
+
+    fn human_name(&self) -> String {
+        "Missile Duel".into()
+    }
+
+    fn init(&mut self, sim: &mut Simulation, seed: u32) {
+        add_walls(sim);
+
+        let mut rng = new_rng(seed);
+        let angle = rng.gen_range(0.0..TAU);
+        let rot = Rotation2::new(angle);
+        let distance = rng.gen_range(4000.0..6000.0);
+
+        ship::create(
+            sim,
+            rot.transform_vector(&vector![-0.5, 0.0]) * distance,
+            vector![0.0, 0.0],
+            0.0,
+            fighter_without_guns(0),
+        );
+        ship::create(
+            sim,
+            rot.transform_vector(&vector![0.5, 0.0]) * distance,
+            vector![0.0, 0.0],
+            std::f64::consts::PI,
+            fighter_without_guns(1),
         );
     }
 
