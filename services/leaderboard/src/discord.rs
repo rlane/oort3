61c59e2efa1cba9fd4a03c552459c262425ae3c2
Rich Lane <richlane@google.com>
2022-11-24 20:25:29 -0800

    leaderboard: handle multiple discord ready() calls

--- services/leaderboard/src/discord.rs
+++ services/leaderboard/src/discord.rs
@@ -5,6 +5,7 @@ use serenity::async_trait;
 use serenity::model::gateway::Ready;
 use serenity::model::id::ChannelId;
 use serenity::prelude::*;
+use std::sync::Arc;
 use tokio::sync::Mutex;
 
 fn channel_id() -> ChannelId {
@@ -23,7 +24,7 @@ pub struct Msg {
 }
 
 struct Handler {
-    rx: Mutex<Option<mpsc::Receiver<Msg>>>,
+    rx: Arc<Mutex<mpsc::Receiver<Msg>>>,
 }
 
 #[async_trait]
@@ -31,20 +32,17 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         log::info!("Connected to Discord as {}", ready.user.name);
 
-        if let Some(mut rx) = self.rx.lock().await.take() {
-            tokio::spawn(async move {
-                while let Some(msg) = rx.recv().await {
-                    log::info!("Sending Discord message {:?}", msg.text);
-                    if let Err(e) = channel_id().say(&ctx.http, &msg.text).await {
-                        log::error!("Error sending message: {:?}", e);
-                    }
-                    // Rate limit messages.
-                    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+        let rx = self.rx.clone();
+        tokio::spawn(async move {
+            while let Some(msg) = rx.lock().await.recv().await {
+                log::info!("Sending Discord message {:?}", msg.text);
+                if let Err(e) = channel_id().say(&ctx.http, &msg.text).await {
+                    log::error!("Error sending message: {:?}", e);
                 }
-            });
-        } else {
-            panic!("RX channel already taken");
-        }
+                // Rate limit messages.
+                tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+            }
+        });
     }
 }
 
@@ -63,7 +61,7 @@ pub async fn start() -> Result<mpsc::Sender<Msg>, anyhow::Error> {
     // by Discord for bot users.
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler {
-            rx: Mutex::new(Some(rx)),
+            rx: Arc::new(Mutex::new(rx)),
         })
         .await
         .expect("Err creating client");
