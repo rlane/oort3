e27bb5bdc8125c5c623c16325a7bd7f51cf20d36
Rich Lane <richlane@google.com>
2022-11-27 19:12:51 -0800

    leaderboard: pass envelope secret through docker build

--- scripts/build-leaderboard-service-docker-image.sh
+++ scripts/build-leaderboard-service-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
 (cd services && cargo verify-project --frozen --locked)
-DOCKER_BUILDKIT=1 docker build -f services/leaderboard/Dockerfile --tag oort_leaderboard_service .
+DOCKER_BUILDKIT=1 docker build -f services/leaderboard/Dockerfile --tag oort_leaderboard_service --build-arg OORT_ENVELOPE_SECRET=${OORT_ENVELOPE_SECRET} .
