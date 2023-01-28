a9708924fb746c09d9a26aef743cd7a0ddcc689b
Rich Lane <richlane@google.com>
2023-01-28 00:06:58 -0800

    services: fix plumbing OORT_CODE_ENCRYPTION_SECRET

--- scripts/build-leaderboard-service-docker-image.sh
+++ scripts/build-leaderboard-service-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 cd $(realpath $(dirname $0))/..
 (cd services && cargo verify-project --frozen --locked)
-DOCKER_BUILDKIT=1 docker build -f services/leaderboard/Dockerfile --tag oort_leaderboard_service --build-arg OORT_ENVELOPE_SECRET=${OORT_ENVELOPE_SECRET}  --build-arg OORT_CODE_ENCRYPTION_SECRET=${OORT_CODE_ENCRYPTION_SECRET} .
+DOCKER_BUILDKIT=1 docker build -f services/leaderboard/Dockerfile --tag oort_leaderboard_service --build-arg OORT_ENVELOPE_SECRET=${OORT_ENVELOPE_SECRET} --build-arg OORT_CODE_ENCRYPTION_SECRET=${OORT_CODE_ENCRYPTION_SECRET} .
