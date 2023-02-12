28f2f4a65bf3dbd40b7112b96538521e9e43eed0
Rich Lane <richlane@google.com>
2023-02-11 21:58:06 -0800

    tools: add prepare-compiler-directory

---
+++ tools/src/bin/prepare-compiler-directory.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use clap::Parser as _;
+
+fn main() -> Result<()> {
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or("prepare_compiler_directory=info"),
+    )
+    .init();
+
+    #[derive(clap::Parser, Debug)]
+    struct Arguments {
+        #[clap(short, long, default_value = "/tmp/oort-ai")]
+        dir: String,
+    }
+    let args = Arguments::parse();
+
+    std::fs::remove_dir_all(&args.dir)?;
+    std::fs::create_dir_all(&args.dir)?;
+    let mut compiler = oort_compiler::Compiler::new_with_dir(std::path::Path::new(&args.dir));
+    compiler.compile(include_str!("../../../shared/ai/empty.rs"))?;
+    Ok(())
+}
