04aea5624c1754b512705ddc23ad91a7262ed317
Rich Lane <richlane@google.com>
2023-02-18 19:54:53 -0800

    sim: add golden_test

---
+++ shared/simulator/tests/golden_test.rs
@@ -0,0 +1,21 @@
+use oort_simulator::scenario;
+use oort_simulator::simulation;
+use test_log::test;
+
+fn run(scenario_name: &str) -> u64 {
+    let scenario = scenario::load(scenario_name);
+    let codes = scenario.solution_codes();
+    let seed = 0;
+    let mut sim = simulation::Simulation::new(scenario_name, seed, &codes);
+
+    while sim.status() == scenario::Status::Running {
+        sim.step();
+    }
+
+    sim.hash()
+}
+
+#[test]
+fn test_frigate_vs_cruiser() {
+    assert_eq!(run("frigate_vs_cruiser"), 15044307371063929937);
+}
