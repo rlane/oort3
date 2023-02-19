328cee40a25403c73291a03f2bf199736e053cdf
Rich Lane <richlane@google.com>
2023-02-19 01:58:35 -0800

    sim: accelerate bullets with CoarseGrid

--- shared/simulator/src/bullet.rs
+++ shared/simulator/src/bullet.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
 use super::index_set::{HasIndex, Index};
-use crate::collision;
-use crate::simulation::{Simulation, PHYSICS_TICK_LENGTH};
+use crate::simulation::{Simulation, PHYSICS_TICK_LENGTH, WORLD_SIZE};
+use crate::{collision, simulation};
+use bitvec::vec::BitVec;
 use nalgebra::{Vector2, Vector4};
 use rapier2d_f64::prelude::*;
 use static_aabb2d_index::*;
@@ -85,7 +86,7 @@ pub fn destroy(sim: &mut Simulation, handle: BulletHandle) {
 
 pub fn tick(sim: &mut Simulation) {
     let dt = PHYSICS_TICK_LENGTH;
-    let indices_by_team = build_indices(sim, dt);
+    let (indices_by_team, coarse_grids_by_team) = build_indices(sim, dt);
     let mut stack = Vec::new();
     let shape = rapier2d_f64::geometry::Ball { radius: 1.0 };
     let bullets: Vec<BulletHandle> = sim.bullets.iter().cloned().collect();
@@ -101,29 +102,45 @@ pub fn tick(sim: &mut Simulation) {
         };
 
         let has_collider;
+        let coarse_grid_hit;
         let mut needs_collider = false;
         {
             let body = sim.bodies.get_mut(RigidBodyHandle(handle.index())).unwrap();
             has_collider = !body.colliders().is_empty();
 
-            let aabb = shape.compute_swept_aabb(
-                body.position(),
-                &body.predict_position_using_velocity_and_forces(dt),
-            );
+            let position = *body.translation();
+            if position.x < -WORLD_SIZE / 2.0
+                || position.x > WORLD_SIZE / 2.0
+                || position.y < -WORLD_SIZE / 2.0
+                || position.y > WORLD_SIZE / 2.0
+            {
+                destroy(sim, handle);
+                continue;
+            }
 
-            for (other_team, index) in indices_by_team.iter() {
-                if team != *other_team {
-                    needs_collider = needs_collider
-                        || index
-                            .query_iter_with_stack(
-                                aabb.mins.x,
-                                aabb.mins.y,
-                                aabb.maxs.x,
-                                aabb.maxs.y,
-                                &mut stack,
-                            )
-                            .next()
-                            .is_some();
+            coarse_grid_hit = coarse_grids_by_team
+                .iter()
+                .any(|(other_team, grid)| *other_team != team && grid.lookup(position));
+            if coarse_grid_hit {
+                let aabb = shape.compute_swept_aabb(
+                    body.position(),
+                    &body.predict_position_using_velocity_and_forces(dt),
+                );
+
+                for (other_team, index) in indices_by_team.iter() {
+                    if team != *other_team {
+                        needs_collider = needs_collider
+                            || index
+                                .query_iter_with_stack(
+                                    aabb.mins.x,
+                                    aabb.mins.y,
+                                    aabb.maxs.x,
+                                    aabb.maxs.y,
+                                    &mut stack,
+                                )
+                                .next()
+                                .is_some();
+                    }
                 }
             }
         }
@@ -133,11 +150,28 @@ pub fn tick(sim: &mut Simulation) {
         } else if has_collider && !needs_collider {
             remove_collider(sim, handle);
         }
+
+        if COLOR_COLLIDERS {
+            if needs_collider {
+                data_mut(sim, handle).color = vector![0.0, 1.0, 0.0, 1.0];
+            } else if coarse_grid_hit {
+                data_mut(sim, handle).color = vector![0.0, 0.0, 1.0, 1.0];
+            } else {
+                data_mut(sim, handle).color = vector![1.0, 0.0, 0.0, 1.0];
+            }
+        }
     }
 }
 
-fn build_indices(sim: &Simulation, dt: f64) -> HashMap<i32, StaticAABB2DIndex<f64>> {
+fn build_indices(
+    sim: &Simulation,
+    dt: f64,
+) -> (
+    HashMap<i32, StaticAABB2DIndex<f64>>,
+    HashMap<i32, CoarseGrid>,
+) {
     let mut aabbs_by_team: HashMap<i32, Vec<Aabb>> = HashMap::new();
+    let mut coarse_grids_by_team: HashMap<i32, CoarseGrid> = HashMap::new();
 
     for handle in sim.ships.iter() {
         let body = sim.ship(*handle).body();
@@ -147,6 +181,10 @@ fn build_indices(sim: &Simulation, dt: f64) -> HashMap<i32, StaticAABB2DIndex<f6
             collider.compute_swept_aabb(&body.predict_position_using_velocity_and_forces(dt));
         let team = sim.ship(*handle).data().team;
         aabbs_by_team.entry(team).or_default().push(aabb);
+        coarse_grids_by_team
+            .entry(team)
+            .or_default()
+            .insert(*body.translation());
     }
 
     let mut indices_by_team: HashMap<i32, StaticAABB2DIndex<f64>> = HashMap::new();
@@ -157,7 +195,8 @@ fn build_indices(sim: &Simulation, dt: f64) -> HashMap<i32, StaticAABB2DIndex<f6
         }
         indices_by_team.insert(team, builder.build().unwrap());
     }
-    indices_by_team
+
+    (indices_by_team, coarse_grids_by_team)
 }
 
 fn add_collider(sim: &mut Simulation, handle: BulletHandle) {
@@ -176,9 +215,6 @@ fn add_collider(sim: &mut Simulation, handle: BulletHandle) {
 }
 
 fn remove_collider(sim: &mut Simulation, handle: BulletHandle) {
-    if COLOR_COLLIDERS {
-        data_mut(sim, handle).color = vector![1.0, 0.0, 0.0, 1.0];
-    }
     let colliders = sim
         .bodies
         .get_mut(RigidBodyHandle(handle.index()))
@@ -192,5 +228,53 @@ fn remove_collider(sim: &mut Simulation, handle: BulletHandle) {
             &mut sim.bodies,
             true,
         );
+    }
+}
+
+struct CoarseGrid {
+    cells: BitVec,
+}
+
+impl CoarseGrid {
+    const CELL_SIZE: f64 = 100.0;
+    const RECIP_CELL_SIZE: f64 = 1.0 / Self::CELL_SIZE;
+    const WORLD_WIDTH: f64 = simulation::WORLD_SIZE + Self::CELL_SIZE * 2.0;
+    const WIDTH: i32 = (Self::WORLD_WIDTH / Self::CELL_SIZE) as i32;
+
+    fn to_cell(p: Vector2<f64>) -> usize {
+        let x = ((Self::WORLD_WIDTH / 2.0 + p.x) * Self::RECIP_CELL_SIZE) as i32;
+        let y = ((Self::WORLD_WIDTH / 2.0 + p.y) * Self::RECIP_CELL_SIZE) as i32;
+        (Self::WIDTH * y + x) as usize
+    }
+
+    pub fn new() -> Self {
+        let mut cells = BitVec::new();
+        cells.resize((Self::WIDTH * Self::WIDTH) as usize, false);
+        Self { cells }
+    }
+
+    pub fn lookup(&self, p: Vector2<f64>) -> bool {
+        let index: usize = Self::to_cell(p);
+        if index < self.cells.len() {
+            self.cells[index]
+        } else {
+            false
+        }
+    }
+
+    pub fn insert(&mut self, p: Vector2<f64>) {
+        let cell = Self::to_cell(p);
+        for xo in [-1, 0, 1] {
+            for yo in [-Self::WIDTH, 0, Self::WIDTH] {
+                let index = (cell as i32 + xo + yo) as usize;
+                self.cells.set(index, true);
+            }
+        }
+    }
+}
+
+impl Default for CoarseGrid {
+    fn default() -> Self {
+        Self::new()
     }
 }
--- shared/simulator/tests/golden_test.rs
+++ shared/simulator/tests/golden_test.rs
@@ -17,5 +17,5 @@ fn run(scenario_name: &str) -> u64 {
 
 #[test]
 fn test_frigate_vs_cruiser() {
-    assert_eq!(run("frigate_vs_cruiser"), 18252782732018435727);
+    assert_eq!(run("frigate_vs_cruiser"), 7652623512616226250);
 }
