06174a0765bac556ffe30ff93fa3484de30e889e
Rich Lane <richlane@google.com>
2023-02-20 22:20:31 -0800

    ui: fix status display when animation frames are paused

--- frontend/app/src/ui/mod.rs
+++ frontend/app/src/ui/mod.rs
@@ -233,7 +233,7 @@ impl UI {
             }
         }
 
-        if self.frame % 10 == 0 {
+        if self.frame % 10 == 0 || self.paused || self.status != Status::Running {
             status_msgs.push(format!("{:.0} fps", self.fps.fps()));
             if self.debug {
                 let (a, b, c) = self.frame_timer.get_latency();
