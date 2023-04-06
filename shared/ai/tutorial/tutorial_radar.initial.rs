de0b4adf2d34f2c39fcb8b340df424d55dd89795
Rich Lane <richlane@google.com>
2023-04-05 21:03:07 -0700

    ai: use radar_width() and advertise single-step in tutorial_radar

--- shared/ai/tutorial/tutorial_radar.initial.rs
+++ shared/ai/tutorial/tutorial_radar.initial.rs
@@ -1,6 +1,7 @@
 // Tutorial: Radar
 // Destroy the enemy ships. Use your radar to find them.
 // Hint: Press 'g' in-game to show where your radar is looking.
+// Hint: Press 'n' to single-step.
 // Hint: Use the set_radar_heading() function to keep your radar pointed at a
 // target, or to search for a new one.
 //
@@ -16,7 +17,7 @@ impl Ship {
     }
 
     pub fn tick(&mut self) {
-        set_radar_heading(radar_heading() + TAU / 6.0);
+        set_radar_heading(radar_heading() + radar_width());
         if let Some(contact) = scan() {
             accelerate(0.1 * (contact.position - position()));
             fire(0);
