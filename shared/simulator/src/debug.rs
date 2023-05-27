5eadb4d4ccb82ff43efbda7988a859e8a3f5554f
Rich Lane <richlane@google.com>
2023-05-27 09:20:53 -0700

    sim: draw turret heading debug lines

--- shared/simulator/src/debug.rs
+++ shared/simulator/src/debug.rs
@@ -1,6 +1,6 @@
 use crate::ship::ShipHandle;
 use crate::simulation::Simulation;
-use nalgebra::{vector, Point2, Vector4};
+use nalgebra::{vector, Point2, UnitComplex, Vector4};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -32,5 +32,18 @@ pub fn emit_ship(sim: &mut Simulation, handle: ShipHandle) {
             .transform_vector(&sim.ship(handle).data().acceleration),
         color: vector![0.0, 1.0, 0.2, 1.0],
     });
+    for gun in sim.ship(handle).data().guns.iter() {
+        if gun.min_angle == gun.max_angle {
+            continue;
+        }
+        let turret_rot = UnitComplex::new(gun.heading);
+        let p0 = p + body.rotation().transform_vector(&gun.offset);
+        let p1 = p0 + turret_rot.transform_vector(&vector![10.0, 0.0]);
+        lines.push(Line {
+            a: p0,
+            b: p1,
+            color: vector![1.0, 0.0, 0.0, 1.0],
+        });
+    }
     sim.emit_debug_lines(handle, lines);
 }
