8d13125302756af86d2feac89c76fe7de542513c
Rich Lane <richlane@google.com>
2023-05-28 17:50:50 -0700

    ai: make planetary_defense enemy more interesting

--- shared/ai/planetary_defense.enemy.rs
+++ shared/ai/planetary_defense.enemy.rs
@@ -1,18 +1,28 @@
 use oort_api::prelude::*;
 
-pub struct Ship {}
+pub struct Ship {
+    course_correction_time: f64,
+}
 
 impl Ship {
     pub fn new() -> Ship {
-        Ship {}
+        Ship {
+            course_correction_time: current_time() + rand(0.0, 10.0),
+        }
     }
 
     pub fn tick(&mut self) {
+        if current_time() < self.course_correction_time {
+            return;
+        }
         let target = vec2(0.0, -17500.0);
         let dp = target - position();
         let err = velocity().normalize() - dp.normalize();
-        let acc =
+        let mut acc =
             dp.normalize() * max_forward_acceleration() - err * 10.0 * max_lateral_acceleration();
+        if velocity().length() > 2000.0 {
+            acc -= velocity();
+        }
         turn(angle_diff(heading(), acc.angle()));
         accelerate(acc);
     }
