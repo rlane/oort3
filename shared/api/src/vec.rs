9bbc7ce76050905bc51bb31027e58ff35129fe95
Rich Lane <richlane@google.com>
2023-09-06 12:23:35 -0700

    api: replace Vec2 with maths_rs version

--- shared/api/src/vec.rs
+++ shared/api/src/vec.rs
@@ -1,55 +1,50 @@
-use std::{
-    fmt::Display,
-    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
-};
-
 /// A two-dimensional vector.
-#[derive(Clone, Copy, Debug, Default, PartialEq)]
-pub struct Vec2 {
-    /// The X coordinate.
-    pub x: f64,
-    /// The Y coordinate.
-    pub y: f64,
+pub type Vec2 = maths_rs::vec::Vec2<f64>;
+
+/// Returns a [Vec2] with the given coordinates.
+pub fn vec2(x: f64, y: f64) -> Vec2 {
+    Vec2::new(x, y)
 }
 
-impl Vec2 {
-    /// Constructs a [Vec2].
-    pub fn new(x: f64, y: f64) -> Vec2 {
-        Vec2 { x, y }
-    }
-
-    /// Broadcast a scalar into a [Vec2].
-    pub fn splat(x: f64) -> Vec2 {
-        Vec2 { x, y: x }
-    }
-
+/// Extra methods for Vec2.
+pub trait Vec2Extras {
     /// Returns the length (or distance from origin).
-    pub fn length(self) -> f64 {
+    fn length(self) -> f64;
+
+    /// Returns a normalized vector with the same direction but length of 1.
+    fn normalize(self) -> Vec2;
+
+    /// Returns the distance to `other`.
+    fn distance(self, other: Vec2) -> f64;
+
+    /// Returns the dot product with `other`.
+    fn dot(self, other: Vec2) -> f64;
+
+    /// Returns the angle of the vector (in radians).
+    fn angle(self) -> f64;
+
+    /// Returns this vector rotated by the given angle (in radians).
+    fn rotate(self, angle: f64) -> Vec2;
+}
+
+impl Vec2Extras for Vec2 {
+    fn length(self) -> f64 {
         (self.x * self.x + self.y * self.y).sqrt()
     }
 
-    /// Returns a normalized vector with the same direction but length of 1.
-    pub fn normalize(self) -> Vec2 {
+    fn normalize(self) -> Vec2 {
         self / self.length()
     }
 
-    /// Returns the distance to `other`.
-    pub fn distance(self, other: Vec2) -> f64 {
+    fn distance(self, other: Vec2) -> f64 {
         (self - other).length()
     }
 
-    /// Returns the dot product with `other`.
-    pub fn dot(self, other: Vec2) -> f64 {
+    fn dot(self, other: Vec2) -> f64 {
         self.x * other.x + self.y * other.y
     }
 
-    /// Returns the cross product with `other`.
-    pub fn cross(self, other: Vec2) -> f64 {
-        self.x * other.y - other.x * self.y
-    }
-
-    /// Returns the angle of the vector (in radians).
-    pub fn angle(self) -> f64 {
+    fn angle(self) -> f64 {
         let mut a = self.y.atan2(self.x);
         if a < 0.0 {
             a += std::f64::consts::TAU;
@@ -57,8 +52,7 @@ impl Vec2 {
         a
     }
 
-    /// Returns this vector rotated by the given angle (in radians).
-    pub fn rotate(self, angle: f64) -> Vec2 {
+    fn rotate(self, angle: f64) -> Vec2 {
         let cos = angle.cos();
         let sin = angle.sin();
         Vec2 {
@@ -66,105 +60,4 @@ impl Vec2 {
             y: self.x * sin + self.y * cos,
         }
     }
-}
-
-impl Display for Vec2 {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        fmt.debug_tuple("").field(&self.x).field(&self.y).finish()
-    }
-}
-
-impl Add for Vec2 {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-
-impl AddAssign for Vec2 {
-    fn add_assign(&mut self, other: Self) {
-        *self = *self + other;
-    }
-}
-
-impl Sub for Vec2 {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        }
-    }
-}
-
-impl SubAssign for Vec2 {
-    fn sub_assign(&mut self, other: Self) {
-        *self = *self - other;
-    }
-}
-
-impl Mul<f64> for Vec2 {
-    type Output = Self;
-
-    fn mul(self, other: f64) -> Self {
-        Self {
-            x: self.x * other,
-            y: self.y * other,
-        }
-    }
-}
-
-impl Mul<Vec2> for f64 {
-    type Output = Vec2;
-
-    fn mul(self, other: Vec2) -> Vec2 {
-        Vec2 {
-            x: self * other.x,
-            y: self * other.y,
-        }
-    }
-}
-
-impl MulAssign<f64> for Vec2 {
-    fn mul_assign(&mut self, other: f64) {
-        *self = *self * other;
-    }
-}
-
-impl Div<f64> for Vec2 {
-    type Output = Self;
-
-    fn div(self, other: f64) -> Self {
-        Self {
-            x: self.x / other,
-            y: self.y / other,
-        }
-    }
-}
-
-impl DivAssign<f64> for Vec2 {
-    fn div_assign(&mut self, other: f64) {
-        *self = *self / other;
-    }
-}
-
-impl Neg for Vec2 {
-    type Output = Self;
-
-    fn neg(self) -> Self {
-        Self {
-            x: -self.x,
-            y: -self.y,
-        }
-    }
-}
-
-/// Returns a [Vec2] with the given coordinates.
-pub fn vec2(x: f64, y: f64) -> Vec2 {
-    Vec2::new(x, y)
 }
