77a018260fb646b3eed8c80b593ee99b0f4bce70
Rich Lane <richlane@google.com>
2023-09-18 19:11:41 -0700

    tools: diff ships fields in determinism

--- tools/src/bin/determinism.rs
+++ tools/src/bin/determinism.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use oort_simulator::snapshot::Snapshot;
 use oort_simulator::{scenario, simulation};
 use std::default::Default;
 use std::path::PathBuf;
@@ -48,10 +49,65 @@ async fn main() -> anyhow::Result<()> {
         let hashes = sims.iter().map(|sim| sim.hash()).collect::<Vec<_>>();
         if hashes[0] != hashes[1] {
             println!("hashes differ at tick {}", sims[0].tick());
+            diff_snapshots(&sims[0].snapshot(0), &sims[1].snapshot(0));
             break;
         }
         sims.iter_mut().for_each(|sim| sim.step());
     }
 
     Ok(())
+}
+
+fn diff_snapshots(a: &Snapshot, b: &Snapshot) {
+    if a.ships.len() != b.ships.len() {
+        println!("ship count differs");
+        println!("  a: {}", a.ships.len());
+        println!("  b: {}", b.ships.len());
+    }
+
+    for (i, (a, b)) in a.ships.iter().zip(b.ships.iter()).enumerate() {
+        let epsilon = 0.0;
+
+        if (a.position - b.position).magnitude() > epsilon {
+            println!("ship {} position differs", i);
+            println!("  a: {:?}", a.position);
+            println!("  b: {:?}", b.position);
+        }
+
+        if (a.velocity - b.velocity).magnitude() > epsilon {
+            println!("ship {} velocity differs", i);
+            println!("  a: {:?}", a.velocity);
+            println!("  b: {:?}", b.velocity);
+        }
+
+        if (a.acceleration - b.acceleration).magnitude() > epsilon {
+            println!("ship {} acceleration differs", i);
+            println!("  a: {:?}", a.acceleration);
+            println!("  b: {:?}", b.acceleration);
+        }
+
+        if (a.heading - b.heading).abs() > epsilon {
+            println!("ship {} heading differs", i);
+            println!("  a: {:?}", a.heading);
+            println!("  b: {:?}", b.heading);
+        }
+
+        if (a.angular_velocity - b.angular_velocity).abs() > epsilon {
+            println!("ship {} angular_velocity differs", i);
+            println!("  a: {:?}", a.angular_velocity);
+            println!("  b: {:?}", b.angular_velocity);
+        }
+
+        if (a.health - b.health).abs() > epsilon {
+            println!("ship {} health differs", i);
+            println!("  a: {:?}", a.health);
+            println!("  b: {:?}", b.health);
+        }
+
+        if a.fuel != b.fuel {
+            println!("ship {} fuel differs", i);
+            println!("  a: {:?}", a.fuel);
+            println!("  b: {:?}", b.fuel);
+        }
+    }
 }
