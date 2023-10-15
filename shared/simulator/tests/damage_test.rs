7e5fd20bfcd9d5ae4981ae97ffb94310a032e8dd
Rich Lane <richlane@google.com>
2023-10-14 20:02:19 -0700

    sim: move center of mass to ship position

--- shared/simulator/tests/damage_test.rs
+++ shared/simulator/tests/damage_test.rs
@@ -25,7 +25,7 @@ fn find_gun_dps(mut ship_data: ship::ShipData, gun: i64) -> f64 {
     );
     let ship1 = ship::create(
         &mut sim,
-        vector![100.0, 0.0] + offset,
+        vector![200.0, 0.0] + offset,
         vector![0.0, 0.0],
         0.0,
         high_health_target(1),
