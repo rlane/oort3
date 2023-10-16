836c0501a4413ddb8bb6c8d3dcb9173a5d5756b6
Rich Lane <richlane@google.com>
2023-10-15 17:18:34 -0700

    sim: enable enhanced-determinism for rapier

--- shared/simulator/tests/golden_test.rs
+++ shared/simulator/tests/golden_test.rs
@@ -17,5 +17,5 @@ fn run(scenario_name: &str) -> u64 {
 
 #[test]
 fn test_frigate_vs_cruiser() {
-    assert_eq!(run("frigate_vs_cruiser"), 17819292810102175441);
+    assert_eq!(run("frigate_vs_cruiser"), 12731786239136837248);
 }
