7677ac3ae0f898297bfae768c3ef359317722033
Rich Lane <richlane@google.com>
2023-10-24 19:15:47 -0700

    api: simplify internals

--- shared/ai/src/lib.rs
+++ shared/ai/src/lib.rs
@@ -1,51 +1,34 @@
 mod user;
 
-use std::collections::HashMap;
-
 // For compatibility.
 pub use oort_api::prelude;
 
-struct ShipWrapper {
-    user_ship: user::Ship,
-    rng: oort_api::rng_state::RngState,
-}
+use std::sync::Once;
+use user::Ship;
 
-static mut SHIPS: Option<HashMap<i32, ShipWrapper>> = None;
-
-#[doc(hidden)]
-#[no_mangle]
-unsafe fn export_initialize() {
-    oort_api::panic::install();
-    unsafe {
-        SHIPS = Some(HashMap::new());
-    }
-}
+static START: Once = Once::new();
+static mut SHIP: Option<Ship> = None;
 
 #[doc(hidden)]
 #[no_mangle]
-pub unsafe fn export_tick_ship(key: i32) {
+unsafe fn export_initialize() {}
+
+#[doc(hidden)]
+#[no_mangle]
+pub unsafe fn export_tick_ship(_key: i32) {
+    START.call_once(|| {
+        oort_api::panic::install();
+        oort_api::rng_state::set(oort_api::rng_state::RngState::new());
+    });
     oort_api::dbg::reset();
     oort_api::panic::reset();
     unsafe {
-        let ship = SHIPS.as_mut().unwrap().entry(key).or_insert_with(|| {
-            let rng = oort_api::rng_state::RngState::new();
-            oort_api::rng_state::set(rng.clone());
-            ShipWrapper {
-                user_ship: user::Ship::new(),
-                rng,
-            }
-        });
-        oort_api::rng_state::set(ship.rng.clone());
-        ship.user_ship.tick();
+        let ship = SHIP.get_or_insert_with(Ship::new);
+        ship.tick();
         oort_api::dbg::update();
-        ship.rng = oort_api::rng_state::get().clone();
     }
 }
 
 #[doc(hidden)]
 #[no_mangle]
-pub unsafe fn export_delete_ship(key: i32) {
-    unsafe {
-        SHIPS.as_mut().unwrap().remove(&key);
-    }
-}
+pub unsafe fn export_delete_ship(_key: i32) {}
